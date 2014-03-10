c55defa137699d33e72b635cd3dd6ac1a17f743e
jlchereau <jlchereau@msn.com>
2014-03-10 11:33:26 +0000

    Test of models and datasources

---
+++ test/kidoju.models.test.js
@@ -0,0 +1,14 @@
+/**
+ * Created by jlchereau on 10/03/14.
+ */
+
+$(document).ready(function() {
+
+    module('Test page loaded and widgets initialized as expected');
+    test('HTML controls found', function() {
+        expect(1);
+        ok(true);
+    });
+
+});
+

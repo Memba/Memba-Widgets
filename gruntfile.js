7a608c66da186ef0891031283719f83aaa735bcf
jlchereau <jlchereau@msn.com>
2014-06-16 12:20:02 +0100

    Minor changes

--- gruntfile.js
+++ gruntfile.js
@@ -1,8 +1,14 @@
-//http://gruntjs.com/sample-gruntfile
+/**
+ * Copyright (c) 2013-2014 Memba Sarl. All rights reserved.
+ * Sources at https://github.com/Memba/Kidoju-Platform
+ */
+
+/* jslint node: true */
+/* jshint node: true */
+
+"use strict";
 
 module.exports = function (grunt) {
-
-    "use strict";
 
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
--- nodejs/http.server.js
+++ nodejs/http.server.js
@@ -1,79 +1,51 @@
 //http://www.hongkiat.com/blog/node-js-server-side-javascript/
 //http://stackoverflow.com/questions/6084360/node-js-as-a-simple-web-server
+//https://github.com/requirejs/example-jquery-cdn/blob/master/tools/server.js
 
 //Install: add --debug=8081 in Node parameters
 
 var sys = require("sys"),
-    my_http = require("http"),
+    http = require("http"),
     path = require("path"),
     url = require("url"),
-    filesys = require("fs");
+    fs = require("fs"),
+    port = /*process.argv[2] ||*/ 8080,
+    types = {
+        'html'  : 'text/html',
+        'txt'   : 'text/plain',
+        'css'   : 'text/css',
+        'js'    : 'text/javascript',
+        'jpg'   : 'image/jpeg',
+        'png'   : 'image/png',
+        'gif'   : 'image/gif',
+        'svg'   : 'image/svg+xml',
+        'json'  : 'application/json',
+        'xml'   : 'text/xml',
+        'bin'   : 'application/octet-stream'
+    };
 
-my_http.createServer(function(request,response){
+http.createServer(function(request,response){
     //debugger;
-    sys.puts(request.url);
-    var my_path = url.parse(request.url).pathname;
-    var full_path = path.join(process.cwd(),my_path);
-    sys.puts(full_path);
-    //path.exists(full_path,function(exists){
-    filesys.exists(full_path,function(exists){
+    var uri = url.parse(request.url).pathname,
+        filename = path.join(process.cwd(),uri);
+    sys.puts(request.url); //basic console.log
+    sys.puts(filename);
+    fs.exists(filename,function(exists){
         if(!exists){
             response.writeHeader(404, {'Content-Type': "text/plain"});
             response.write("404 Not Found\n");
             response.end();
+            return;
         }
-        else{
-            filesys.readFile(full_path, 'binary', function(err, file) {
-                if(err) {
-                    response.writeHeader(500, {'Content-Type': 'text/plain'});
-                    response.write(err + '\n');
-                    response.end();
-
-                }
-                else{
-                    switch(path.extname(full_path)){
-                        case '.html':
-                        case '.htm':
-                            response.writeHeader(200, {'Content-Type': 'text/html'});
-                            break;
-                        case '.txt':
-                            response.writeHeader(200, {'Content-Type': 'text/plain'});
-                            break;
-                        case '.css':
-                            response.writeHeader(200, {'Content-Type': 'text/css'});
-                            break;
-                        case '.js':
-                            response.writeHeader(200, {'Content-Type': 'text/javascript'});
-                            break;
-                        case '.jpg':
-                            response.writeHeader(200, {'Content-Type': 'image/jpeg'});
-                            break;
-                        case '.png':
-                            response.writeHeader(200, {'Content-Type': 'image/png'});
-                            break;
-                        case '.gif':
-                            response.writeHeader(200, {'Content-Type': 'image/gif'});
-                            break;
-                        case '.svg':
-                            response.writeHeader(200, {'Content-Type': 'image/svg+xml'});
-                            break;
-                        case '.json':
-                            response.writeHeader(200, {'Content-Type': 'application/json'});
-                            break;
-                        case '.xml':
-                            response.writeHeader(200, {'Content-Type': 'text/xml'});
-                            break;
-                        default:
-                            response.writeHeader(200, {'Content-Type': 'application/octet-stream'});
-                            break;
-                    }
-
-                    response.write(file, "binary");
-                    response.end();
-                }
-
-            });
+        if(fs.lstatSync(filename).isDirectory()) {
+            response.writeHead(301, {'Location': path.join(process.cwd(),'index.html') });
+            response.end();
+            return;
         }
+        var type = filename.split('.');
+        type = type[type.length - 1];
+        response.writeHead(200, { 'Content-Type': (types[type] || types['bin']) + '; charset=utf-8' });
+        fs.createReadStream(filename).pipe(response);
     });
-}).listen(8080);
-sys.puts("Server Running on 8080");			
+}).listen(parseInt(port, 10));
+sys.puts("Server Running on port " + port.toString());

63cdbe664763baaf49e4a86f49282a11402fc365
jlchereau <jlchereau@msn.com>
2015-03-06 14:39:12 +0000

    Upgraded sinonJS from v1.12.2 to v1.13.0

--- test/vendor/sinon-ie.js
+++ test/vendor/sinon-ie.js
@@ -1,5 +1,5 @@
 /**
- * Sinon.JS 1.12.2, 2014/12/12
+ * Sinon.JS 1.13.0, 2015/03/05
  *
  * @author Christian Johansen (christian@cjohansen.no)
  * @author Contributors: https://github.com/cjohansen/Sinon.JS/blob/master/AUTHORS

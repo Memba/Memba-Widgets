050f0a3fe52800dd8718d41a260de34fc14ea6a1
jlchereau <jlchereau@msn.com>
2015-06-10 11:01:49 +0100

    Made update.cmd idempotent

--- src/styles/vendor/kendo/update.cmd
+++ src/styles/vendor/kendo/update.cmd
@@ -5,5 +5,5 @@ del /S /Q mobile\*.css
 del /S /Q web\*.css
 
 REM use -i.bak if you need a backup file before changes
-sed -i s/fonts\/DejaVu\/Deja/Deja/g web/fonts/DejaVu/dejavu.less
-sed -i s/textures/..\/textures/g web/common/inputs.less
+sed -i 's/fonts\/DejaVu\/Deja/Deja/g' web/fonts/DejaVu/dejavu.less
+sed -i 's/"textures/"..\/textures/g' web/common/inputs.less

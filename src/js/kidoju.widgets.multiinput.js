cb39ac8af464f3ba7334c957d7191a971a859ae6
jlchereau <jlchereau@msn.com>
2015-06-10 10:50:28 +0100

    Added from Memba-Widgets https://github.com/Memba/Kidoju-Widgets/issues/29

---
+++ src/js/kidoju.widgets.multiinput.js
@@ -0,0 +1,434 @@
+/**
+ * Copyright (c) 2013-2014 Memba Sarl. All rights reserved.
+ * Sources at https://github.com/Memba
+ */
+
+/* jslint browser: true, jquery: true */
+/* jshint browser: true, jquery: true */
+
+// TODO: copy certain styles (width?, ...) from input to wrapper
+// TODO implement regex match
+// TODO: implement _clear() properly
+
+(function ($, undefined) {
+
+    'use strict';
+
+    // shorten references to variables for uglification
+    var fn = Function,
+        global = fn('return this')(),
+        kendo = global.kendo,
+        ui = kendo.ui,
+        Widget = ui.Widget,
+        ObservableArray = kendo.data.ObservableArray,
+        ns = '.kendoMultiInput',
+
+        //Types
+        //NUMBER = 'number',
+        STRING = 'string',
+
+        //Events
+        CHANGE = 'change',
+        CLICK = 'click' + ns,
+        KEYPRESS = 'keypress' + ns,
+        FOCUSOUT = 'focusout' + ns,
+        MOUSEENTER = 'mouseenter' + ns,
+        MOUSELEAVE = 'mouseleave' + ns,
+        HOVEREVENTS = MOUSEENTER + ' ' + MOUSELEAVE,
+
+        //Templates
+        ID = 'id',
+        LI = 'li',
+        ARIA_DISABLED = 'aria-disabled',
+        ARIA_READONLY = 'aria-readonly',
+        //FOCUSEDCLASS = 'k-state-focused',
+        HOVERCLASS = 'k-state-hover',
+        STATEDISABLED = 'k-state-disabled',
+        DISABLED = 'disabled',
+        READONLY = 'readonly';
+
+        //MODULE = 'MultiInput Widget: ',
+        //DEBUG = true;
+
+    /*******************************************************************************************
+     * MultiInput
+     *******************************************************************************************/
+
+    //TODO: hide k-delete when readonly
+
+    /**
+     * MultiInput (kendoMultiInput)
+     * @class MultiInput
+     * @extend Widget
+     */
+    var MultiInput = Widget.extend({
+
+        /**
+         * Initializes the widget
+         * @method init
+         * @param element
+         * @param options
+         */
+        init: function (element, options) {
+            var that = this;
+            that.ns = ns;
+            Widget.fn.init.call(that, element, options);
+            options = $.extend(options, that.options);
+            that._initValue();
+            that._layout();
+            that.refresh();
+            that._editable(options);
+            kendo.notify(that);
+        },
+
+        /**
+         * Widget options
+         * @property options
+         */
+        options: {
+            name: 'MultiInput',
+            value: null,
+            match: null, //RegExp match like email address, cancels separator
+            readonly: true,
+            separators: ',;' //string of separators
+        },
+
+        /**
+         * Events
+         */
+        events: [
+            CHANGE
+        ],
+
+        /**
+         * Initialization of values from HTML markup
+         * @private
+         */
+        _initValue: function() {
+            var that = this,
+                initialValue = that.element.val(),
+                value = that.options.value || initialValue;
+
+            if ($.type(initialValue) === STRING && initialValue.trim().length) {
+                that._initialValue = initialValue;
+                that.element.val(null);
+            }
+
+            if ($.type(value) === STRING && initialValue.trim().length) {
+                try {
+                    value = $.parseJSON(value);
+                } catch(ex) {
+                    value = [value];
+                }
+            }
+
+            that._oldValues = [];
+            that._values = [];
+
+            if (Array.isArray(value)) {
+                that._oldValues = value;
+                that._values = value;
+            }
+        },
+
+        /**
+         * Value containing an email which can be used to find a Gravatar
+         * @method value
+         * @param value
+         * @return {*}
+         */
+        value: function (value) {
+            var that = this;
+            if (value) {
+                if(!$.isArray(value) && !(value instanceof ObservableArray)) {
+                    throw new TypeError();
+                }
+                if(!compare(value, that._values)) {
+                    that._values = value;
+                    that.refresh();
+                }
+            }
+            else if (value === null) {
+                that._values = [];
+            } else {
+                return that._values;
+            }
+        },
+
+        /**
+         * Builds the widget layout
+         * @method _layout
+         * @private
+         */
+        _layout: function () {
+            var that = this,
+                //options = that.options,
+                element = $(that.element), //the <input> element
+                id = element.attr(ID);
+            that._clear();
+            element.attr({
+                'class': 'k-input',
+                'accesskey': '',
+                'autocomplete': 'off',
+                'tabindex' : 0,
+                'role': 'listbox',
+                'aria-owns': id ? id + '_taglist': ''
+            });
+            element.wrap('<div class="k-multiselect-wrap k-floatwrap" unselectable="on"/>');
+            that._innerWrapper = element.parent();
+            that._innerWrapper.wrap('<div class="k-widget k-multiselect k-header" unselectable="on">');
+            that.wrapper = that._innerWrapper.parent();
+            that.tagList = $('<ul role="listbox" unselectable="on" class="k-reset"/>');
+            if (id) {
+                that.tagList.attr('id', id + '_taglist');
+            }
+            that._innerWrapper.prepend(that.tagList);
+        },
+
+        /**
+         * Toggles between editing modes
+         * @param options
+         * @private
+         */
+        _editable: function(options) {
+            var that = this,
+                disable = options.disable,
+                readonly = options.readonly,
+                wrapper = that.wrapper.off(ns),
+                tagList = that.tagList.off(ns),
+                input = that.element.off(ns);
+
+            if (!readonly && !disable) {
+                wrapper
+                    .removeClass(STATEDISABLED)
+                    .on(HOVEREVENTS, that._toggleHover)
+                    .on(CLICK,  $.proxy(that.focus, that));
+
+                input
+                    .removeAttr(DISABLED)
+                    .removeAttr(READONLY)
+                    .attr(ARIA_DISABLED, false)
+                    .attr(ARIA_READONLY, false)
+                    .on(KEYPRESS, $.proxy(that._onInputKeyPress, that))
+                    .on(FOCUSOUT, $.proxy(that._onInputFocusOut, that));
+
+                tagList
+                    .on(MOUSEENTER, LI, function() { $(this).addClass(HOVERCLASS); })
+                    .on(MOUSELEAVE, LI, function() { $(this).removeClass(HOVERCLASS); })
+                    .on(CLICK, '.k-delete', $.proxy(that._onTagClick, that));
+
+            } else {
+                if (disable) {
+                    wrapper.addClass(STATEDISABLED);
+                } else {
+                    wrapper.removeClass(STATEDISABLED);
+                }
+                input
+                    .attr(DISABLED, disable)
+                    .attr(READONLY, readonly)
+                    .attr(ARIA_DISABLED, disable)
+                    .attr(ARIA_READONLY, readonly);
+            }
+        },
+
+        focus: function() {
+            this.element.focus();
+        },
+
+        readonly: function(readonly) {
+            this._editable({
+                readonly: readonly === undefined ? true : readonly,
+                disable: false
+            });
+        },
+
+        enable: function(enable) {
+            this._editable({
+                readonly: false,
+                disable: !(enable = enable === undefined ? true : enable)
+            });
+        },
+
+        /**
+         * Refreshes the widget
+         * @method refresh
+         */
+        refresh: function() {
+            var that = this;
+            that.tagList.empty();
+            that._addTags(that._values);
+            that._change();
+        },
+
+        /**
+         * Add a tag(s) to display
+         */
+        _addTags: function(values) {
+            var that = this;
+            if (!Array.isArray(values) && !(values instanceof ObservableArray)) {
+                values = [values];
+            }
+            $.each(values, function(index, value) {
+                var tagItem = $(kendo.format(
+                    '<li class="k-button" unselectable="on"><span unselectable="on">{0}</span><span unselectable="on" class="k-icon k-delete">{1}</span></li>',
+                    kendo.htmlEncode(value),
+                    'delete'                         //TODO: translate delete!
+                ));
+                that.tagList.append(tagItem);
+            });
+        },
+
+        /**
+         * Remove a tag from display
+         * @param tag
+         * @private
+         */
+        _removeTag: function(tag) {
+            var that = this,
+                index = tag.index();
+            tag.remove();
+            return index;
+        },
+
+        /**
+         * Trigger a change event if values have changed
+         * @private
+         */
+        _change: function() {
+            var that = this,
+                value = that.value();
+            if (!compare(value, that._oldValues)) {
+                that.trigger(CHANGE);
+                that.element.trigger(CHANGE); // also trigger the DOM change event so any subscriber gets notified
+                that._oldValues = value.slice();
+            }
+        },
+
+        /**
+         * Event handler for clicking a tag delete icon
+         * @param e
+         * @private
+         */
+        _onTagClick: function(e) {
+            var that = this,
+                tag = $(e.target).closest(LI),
+                index = that._removeTag(tag);
+            that._values.splice(index, 1);
+            that._change();
+            //that._placeholder();
+        },
+
+        /**
+         *
+         * @param e
+         * @private
+         */
+        _onInputKeyPress: function(e) {
+            var that = this,
+                separators = that.options.separators || '',
+                code = e.keyCode || e.which;
+            if(separators.indexOf(String.fromCharCode(code)) > -1) {
+                that._fromInputToTag();
+                return false;
+            }
+        },
+
+        /**
+         *
+         * @param e
+         * @private
+         */
+        _onInputFocusOut: function() {
+            this._fromInputToTag();
+        },
+
+        _fromInputToTag: function() {
+            var that = this,
+                input = that.element,
+                value = input.val().trim();
+            if (value.length) {
+                that._values.push(value);
+                that._addTags(value);
+                that._change();
+            }
+            input.val(null);
+        },
+
+        _toggleHover: function(e) {
+            $(e.currentTarget).toggleClass(HOVERCLASS, e.type === 'mouseenter');
+        },
+
+        /**
+         * Clears the DOM from modifications made by the widget
+         * @method _clear
+         * @private
+         */
+        _clear: function() {
+            var that = this,
+                ns = that.ns;
+            //unbind descendant events
+            //$(that.element).find('*').off();
+            //clear element
+            //$(that.element)
+            //    .empty()
+            //    .off()
+            //    .removeClass('k-widget k-multiinput');
+
+            /*
+             element = $(that.element),
+             wrapper = element.parent();
+             that.unbind(CHANGE);
+             //remove wrapper and stars
+             if (wrapper.length > 0 && wrapper[0].tagName.toLowerCase() === SPAN && wrapper.hasClass('k-rating')) {
+             wrapper.find('span.k-rating-star').off().remove();
+             wrapper.off();
+             element.unwrap();
+             element.show();
+             }
+             */
+
+
+        },
+
+        /**
+         * Destroys the widget
+         * @method destroy
+         */
+        destroy: function () {
+            var that = this;
+            that._clear();
+            Widget.fn.destroy.call(this);
+        }
+
+    });
+
+    ui.plugin(MultiInput);
+
+    /**
+     * Compare two arrays of values
+     * @param a
+     * @param b
+     * @returns {boolean}
+     */
+    function compare(a, b) {
+        var length;
+
+        if ((a === null && b !== null) || (a !== null && b === null)) {
+            return false;
+        }
+
+        length = a.length;
+        if (length !== b.length) {
+            return false;
+        }
+
+        while (length--) {
+            if (a[length] !== b[length]) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+} (jQuery));
---
+++ src/js/kidoju.widgets.rating.js
@@ -0,0 +1,280 @@
+//Copyright ©2012-2013 Memba® Sarl. All rights reserved.
+
+/*jslint browser:true, jquery:true*/
+/*jshint browser:true, jquery:true*/
+
+//TODO: check touch interfaces
+//TODO: Add tooltip with value
+//TODO: Display half stars
+//TODO: Should we bind to the DOM change event to be notified when input value changes?????
+
+(function (window, $, undefined) {
+
+    'use strict';
+
+    // shorten references to variables for uglification
+    var fn = Function,
+        global = fn('return this')(),
+        kendo = global.kendo,
+        ui = kendo.ui,
+        Widget = ui.Widget,
+        ns = '.kendoRating',
+        SPAN = 'span',
+        NUMBER = 'number',
+        STAR = '&#x2605;',
+        STAR_O = '&#x2606;',
+        STAR_SELECTOR = 'span.k-rating-star',
+        STATE_HOVER = 'k-state-hover',
+        STATE_SELECTED = 'k-state-selected',
+        STATE_DISABLED = 'k-state-disabled',
+
+        //Rating
+        RATING_MIN = 0,
+        RATING_MAX = 5,
+        RATING_STEP = 1,
+        PRECISION = 3,
+
+        //Events
+        CLICK = 'click' + ns,
+        MOUSEENTER = 'mouseenter' + ns,
+        MOUSELEAVE = 'mouseleave' + ns,
+        HOVEREVENTS = MOUSEENTER + ' ' + MOUSELEAVE,
+        CHANGE = 'change';
+
+    /*******************************************************************************************
+     * Rating
+     * SEE: http://css-tricks.com/star-ratings/
+     * SEE: http://www.fyneworks.com/jquery/star-rating/
+     * SEE: http://www.enfew.com/5-best-jquery-star-rating-plugins-tutorials/
+     *******************************************************************************************/
+
+    /**
+     * Rating (kendoRating)
+     * @class Rating
+     * @extend Widget
+     */
+    var Rating = Widget.extend({
+
+        /**
+         * Initializes the widget
+         * @method init
+         * @param element
+         * @param options
+         */
+        init: function (element, options) {
+            var that = this,
+                input = $(element);
+            input.type = NUMBER;
+            that.ns = ns;
+            options = $.extend({}, {
+                value: parseFloat(input.attr('value') || RATING_MIN),
+                min: parseFloat(input.attr('min') || RATING_MIN),
+                max: parseFloat(input.attr('max') || RATING_MAX),
+                step: parseFloat(input.attr('step') || RATING_STEP),
+                disabled: input.prop('disabled'),
+                readonly: input.prop('readonly')
+            }, options);
+            Widget.fn.init.call(that, element, options);
+            that._layout();
+            that.refresh();
+            kendo.notify(that);
+        },
+
+        /**
+         * Widget events
+         * @property events
+         */
+        events: [
+            CHANGE //Changing the rating value by clicking a star raises the change event
+        ],
+
+        /**
+         * Widget options
+         * @property options
+         */
+        options: {
+            name: 'Rating',
+            value: RATING_MIN,
+            min: RATING_MIN,
+            max: RATING_MAX,
+            step: RATING_STEP
+        },
+
+        /**
+         * Gets a sets the rating value
+         * @method value
+         * @param value
+         * @return {*}
+         */
+        value: function (value) {
+            var that = this,
+                input = that.element,
+                options = that.options;
+            value = parseFloat(value, 10);
+            if (isNaN(value)) {
+                return options.value;
+            } else if (value >= options.min && value <= options.max) {
+                if (options.value !== value) {
+                    options.value = value;
+                    //that.element.prop("value", formatValue(value));
+                    input.val(value);
+                    that.refresh();
+                    that.trigger(CHANGE, { value: value });
+                    that.element.trigger(CHANGE); // also trigger the DOM change event so any subscriber gets notified
+                    //http://stackoverflow.com/questions/4672505/why-does-the-jquery-change-event-not-trigger-when-i-set-the-value-of-a-select-us
+                }
+            } else {
+                throw new RangeError(kendo.format('Expecting a number between {0} and {1}', options.min, options.max));
+            }
+        },
+
+        /**
+         * Builds the widget layout
+         * @method _layout
+         * @private
+         */
+        _layout: function() {
+            var that = this,
+                input = that.element,
+                options = that.options;
+            that._clear();
+            input.wrap('<span class="k-widget k-rating"/>');
+            input.hide();
+            //We need that.wrapper for visible/invisible bindings
+            that.wrapper = input.parent();
+            //Calculate the number of stars
+            var n = round((options.max - options.min)/options.step);  //number of stars
+            //Add stars to the DOM
+            for(var i = 1; i <= n; i++) {
+                that.wrapper.append(kendo.format('<span class="k-rating-star" data-star="{0}">{1}</span>', i, STAR_O));
+            }
+            //Make (non)editable
+            that._editable(options);
+        },
+
+        /**
+         * Toggle between editing modes
+         * @private
+         */
+        _editable: function(options) {
+            var that = this,
+                disabled = options.disabled,
+                readonly = options.readonly,
+                wrapper = that.wrapper;
+            wrapper.find(STAR_SELECTOR).off(ns);
+            if (!readonly && !disabled) {
+                wrapper.removeClass(STATE_DISABLED);
+                wrapper.find(STAR_SELECTOR)
+                    .on(HOVEREVENTS, $.proxy(that._toggleHover, that))
+                    .on(CLICK, $.proxy(that._onStarClick, that));
+            } else {
+                wrapper.addClass(STATE_DISABLED);
+            }
+        },
+
+        /**
+         * Function called by the enabled/disabled bindings
+         * @param enable
+         */
+        enable: function(enable) {
+            this._editable({
+                readonly: false,
+                disabled: !(enable = enable === undefined ? true : enable)
+            });
+        },
+
+        /**
+         * Refreshes the widget
+         * @method refresh
+         */
+        refresh: function(e) {
+            var that = this,
+                options = that.options;
+            if (that.wrapper) {
+                var i = round((that.value() - options.min)/options.step);
+                $.each(that.wrapper.find(STAR_SELECTOR), function(index, element) {
+                    var star = $(element);
+                    if(parseFloat(star.attr('data-star')) <= i) {
+                        star.html(STAR).addClass(STATE_SELECTED);
+                    } else {
+                        star.html(STAR_O).removeClass(STATE_SELECTED);
+                    }
+                });
+            }
+        },
+
+        /**
+         * Event handler for clicking/tapping a star
+         * @param e
+         * @private
+         */
+        _onStarClick: function(e) {
+            var that = this,
+                options = that.options;
+            e.preventDefault();
+            var i = parseFloat($(e.currentTarget).attr('data-star')),
+                value = options.min + i * options.step;
+            that.value(value);
+        },
+
+        /**
+         * EVent handler for hovering stars
+         * @param e
+         * @private
+         */
+        _toggleHover: function(e) {
+            var that = this,
+                i = parseFloat($(e.currentTarget).attr('data-star'));
+            $.each(that.wrapper.find(STAR_SELECTOR), function(index, element) {
+                var star = $(element);
+                if(e.type=== 'mouseenter' && parseFloat(star.attr('data-star')) <= i) {
+                    star.html(STAR).addClass(STATE_HOVER);
+                } else {
+                    star.html(star.hasClass(STATE_SELECTED) ? STAR : STAR_O).removeClass(STATE_HOVER);
+                }
+            });
+        },
+
+        /**
+         * Clears the DOM from modifications made by the widget
+         * @method _clear
+         * @private
+         */
+        _clear: function() {
+            var that = this,
+                input = that.element;
+            //remove wrapper and stars
+            if (that.wrapper) {
+                that.wrapper.find(STAR_SELECTOR).off(ns).remove();
+                input.unwrap();
+                delete that.wrapper;
+                input.show();
+            }
+        },
+
+        /**
+         * Destroys the widget
+         * @method destroy
+         */
+        destroy: function () {
+            var that = this;
+            that._clear();
+            Widget.fn.destroy.call(this);
+        }
+    });
+
+    ui.plugin(Rating);
+
+    /**
+     * rounding numbers for the star rating widget
+     * @method round
+     * @param value {Number}
+     * @return {Number}
+     */
+    function round(value) {
+        value = parseFloat(value, 10);
+        var power = Math.pow(10, PRECISION || 0);
+        return Math.round(value * power) / power;
+    }
+
+} (this, jQuery));

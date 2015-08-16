84cc6cb53539d0a6d3de74e647920ab08478584b
jlchereau <jlchereau@msn.com>
2015-08-16 18:57:11 +0100

    Added library tabstrip

--- src/js/kidoju.widgets.assetmanager.js
+++ src/js/kidoju.widgets.assetmanager.js
@@ -8,7 +8,7 @@
 
 (function (f, define){
     'use strict';
-    define(['./vendor/kendo/kendo.binder', './kidoju.data', './kidoju.tools'], f);
+    define(['./vendor/kendo/kendo.binder', './vendor/kendo/kendo.tabstrip', './vendor/kendo/kendo.listview'], f);
 })(function () {
 
     'use strict';
@@ -18,7 +18,19 @@
         var kendo = window.kendo,
             Widget = kendo.ui.Widget,
             STRING = 'string',
-            WIDGET_CLASS = 'k-widget kj-assetmanager';
+            DELETE = 'delete',
+            UPLOAD = 'upload',
+            NS = '.kendoAssetManager',
+            WIDGET_CLASS = 'k-widget kj-assetmanager',
+            TOOLBAR_TMPL = '<div class="k-widget k-filebrowser-toolbar k-header k-floatwrap">' +
+                '<div class="k-toolbar-wrap">' +
+                    '<div class="k-widget k-upload"><div class="k-button k-button-icontext k-upload-button"><span class="k-icon k-add"></span>#=messages.toolbar.upload#<input type="file" name="file" /></div></div>' +
+                    '<button type="button" class="k-button k-button-icon k-state-disabled"><span class="k-icon k-delete" /></button>&nbsp;' +
+                '</div>' +
+                '<div class="k-tiles-arrange">' +
+                    '<label>#=messages.filter#: <select /></label>' +
+                '</div>' +
+            '</div>';
 
             // EVENTS
             // CLICK = 'click',
@@ -62,16 +74,34 @@
              * @property options
              */
             options: {
-                name: 'AssetManager'
+                name: 'AssetManager',
+                transport: {
+                    read: function(options) {},
+                    coll1: function(options) {},
+                    coll2: function(options) {}
+                },
+                messages: {
+                    filter: 'Filter',
+                    toolbar: {
+                        upload: 'Upload',
+                        delete: 'Delete'
+                    },
+                    transport: {
+                        read: 'Project',
+                        coll1: 'O-Collection',
+                        coll2: 'X-Collection'
+                    }
+                }
             },
 
             /**
              * Widget events
              * @property events
              */
-            //events: [
-            //    CHANGE
-            //],
+            events: [
+                DELETE,
+                UPLOAD
+            ],
 
             /**
              * Builds the widget layout
@@ -81,7 +111,67 @@
                 var that = this;
                 that.wrapper = that.element;
                 that.element.addClass(WIDGET_CLASS);
-                that.element.text('bla bla');
+                that._tabStrip();
+                that._tabContent();
+            },
+
+            _tabStrip: function() {
+                var that = this;
+                // Create as many tabs as there are transports
+                var transports = Object.keys(that.options.transport),
+                    div = $('<div></div>'),
+                    ul = $('<ul></ul>').appendTo(div);
+                for (var i = 0; i < transports.length; i++) {
+                    ul.append('<li' + (transports[i] === 'read' ? ' class="k-state-active"' : '') + '>' + that.options.messages.transport[transports[i]] + '</li>');
+                    div.append('<div></div>');
+                }
+                div.appendTo(that.element);
+                // Set the tabStrip item of the component
+                that.tabStrip = div.kendoTabStrip({
+                    tabPosition: "left",
+                    animation: { open: { effects: "fadeIn" } }
+                }).data('kendoTabStrip');
+            },
+
+            _tabContent: function() {
+                var that = this,
+                    tabStrip = that.tabStrip;
+                that.contentElement = $('<div class="kj-assetmanager-content k-filebrowser"></div>')
+                    .appendTo(tabStrip.contentHolder(0));
+                that._toolbar();
+            },
+
+            _toolbar: function() {
+                var that = this,
+                    template = kendo.template(TOOLBAR_TMPL),
+                    messages = that.options.messages,
+                    arrangeBy = [
+                        { text: messages.orderByName, value: "name" },
+                        { text: messages.orderBySize, value: "size" }
+                    ];
+
+                that.toolbar = $(template({
+                    messages: messages
+                })).appendTo(that.contentElement);
+
+                // TODO : upload
+
+                /*
+                that.arrangeBy = that.toolbar.find(".k-tiles-arrange select")
+                    .kendoDropDownList({
+                        dataSource: arrangeBy,
+                        dataTextField: "text",
+                        dataValueField: "value",
+                        change: function() {
+                            that.orderBy(this.value());
+                        }
+                    })
+                    .data("kendoDropDownList");
+                */
+
+                /*
+                that._attachDropzoneEvents();
+                */
             },
 
             /**

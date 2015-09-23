36b117f221c34f819aa9a95d60321f7555275013
jlchereau <jlchereau@msn.com>
2015-09-23 17:11:59 +0100

    Added window.assert and window.log

---
+++ src/js/window.assert.js
@@ -0,0 +1,64 @@
+/**
+ * Copyright (c) 2013-2015 Memba Sarl. All rights reserved.
+ * Sources at https://github.com/Memba
+ */
+
+/* jshint browser: true, jquery: true */
+/* globals define: false */
+
+(function (f, define){
+    'use strict';
+    define([], f);
+})(function () {
+
+    'use strict';
+
+    (function ($, undefined) {
+
+        var STRING = 'string',
+            UNDEFINED = 'undefined';
+
+        /**
+         * Asserts
+         * Note: Use asserts where unmet conditions are independent from user entries, and
+         * developers should be warned that there is probably something unexpected in their code
+         */
+        var assert = window.assert = $.extend(
+            // By extending assert, we ensure we can call both assert() and assert.ok() for the same result (like in nodeJS)
+            function(test, message) {
+                if (!test) { throw new Error(message); }
+            },
+            {
+                enum: function(array, value, message) { if (array.indexOf(value) === -1) { throw new Error(message); } },
+                equal: function(expected, actual, message) { if (expected !== actual) { throw new Error(message); } },
+                instanceof: function(Class, value, message) { if (!(value instanceof Class)) { throw new Error(message); } },
+                isOptionalObject: function(value, message) { if ($.type(value) !== UNDEFINED && (!$.isPlainObject(value) || $.isEmptyObject(value))) { throw new Error(message); } },
+                isPlainObject: function(value, message) { if (!$.isPlainObject(value) || $.isEmptyObject(value)) { throw new Error(message); } },
+                isUndefined: function(value, message) { if ($.type(value) !== UNDEFINED) { throw new Error(message); } },
+                match: function(rx, value, message) { if ($.type(value) !== STRING || !rx.test(value)) { throw new Error(message); } },
+                ok: function(test, message) { return assert(test, message); },
+                type: function(type, value, message) { if ($.type(value) !== type) { throw new TypeError(message); } }
+            },
+            {
+                messages: {
+                    instanceof: {
+                        default: '`{0}` is expected to be an instance of `{1}`'
+                    },
+                    isPlainObject: {
+                        default: '`{0}` is expected to be a plain object'
+                    },
+                    isUndefined: {
+                        default: '`{0}` is expected to be undefined'
+                    },
+                    type: {
+                        default: '`{0}` is expected to be a(n) `{1}`'
+                    }
+                }
+            }
+        );
+
+    }(window.jQuery));
+
+    return window.kendo;
+
+}, typeof define === 'function' && define.amd ? define : function (_, f){ 'use strict'; f(); });
---
+++ src/js/window.log.js
@@ -0,0 +1,94 @@
+/**
+ * Copyright (c) 2013-2015 Memba Sarl. All rights reserved.
+ * Sources at https://github.com/Memba
+ */
+
+/* jshint browser: true, jquery: true */
+/* globals define: false */
+
+(function (f, define){
+    'use strict';
+    define([], f);
+})(function () {
+
+    'use strict';
+
+    (function ($, undefined) {
+
+        var STRING = 'string',
+            UNDEFINED = 'undefined';
+
+        /**
+         * Log class
+         * @class Log
+         */
+        var Log = window.Log = function(module) {
+
+            this._module = module;
+
+            /**
+             * Generic print function
+             * @param message
+             * @param level
+             * @private
+             */
+            this._print = function(message, level) {
+
+                message = $.type(message) === STRING ? { message: message } : message;
+                level = (/^(debug|info|warn|error|crit)$/i).test(level) ? level.toLowerCase() : 'info';
+
+                var app = window.app,
+                    logEntry = $.extend({}, message, { module: this._module });
+
+                // If we have an app.logger, delegate to app logger
+                if (app && app.logger) {
+                    // TODO ------------------------------------------------------------------------------------
+                    $.noop();
+
+                // Otherwise use plain old window.console.log
+                } else if (window.console && $.isFunction(window.console.log)) {
+                    window.console.log(
+                        logEntry.module + ': ' +
+                        logEntry.message +
+                        (logEntry.data ? ' - ' + JSON.stringify(logEntry.data) : ''));
+                }
+            };
+
+        };
+
+        /**
+         * Print debug message
+         * @param message
+         */
+        Log.prototype.debug = function(message) { return this._print(message, 'debug'); };
+
+        /**
+         * Print info message
+         * @param message
+         */
+        Log.prototype.info = function(message) { return this._print(message, 'info'); };
+
+        /**
+         * Print warn message
+         * @param message
+         */
+        Log.prototype.warn = function(message) { return this._print(message, 'warn'); };
+
+        /**
+         * Print error message
+         * @param message
+         */
+        Log.prototype.error = function(message) { return this._print(message, 'error'); };
+
+        /**
+         * Print critical message
+         * @param message
+         */
+        Log.prototype.crit = function(message) { return this._print(message, 'crit'); };
+
+
+    }(window.jQuery));
+
+    return window.kendo;
+
+}, typeof define === 'function' && define.amd ? define : function (_, f){ 'use strict'; f(); });

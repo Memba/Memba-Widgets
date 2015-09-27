143dc7ecfb331951ff01a3f16e628e323ce7d015
jlchereau <jlchereau@msn.com>
2015-09-27 17:43:05 +0100

    Fixed jscs and jshint errors

--- src/js/kidoju.widgets.quiz.js
+++ src/js/kidoju.widgets.quiz.js
@@ -18,26 +18,26 @@
 
     'use strict';
 
-    (function($, undefined) {
+    (function ($, undefined) {
 
-        var kendo = window.kendo,
-            ui = kendo.ui,
-            Widget = ui.Widget,
-            assert = window.assert,
-            logger = new window.Log('kidoju.widgets.quiz'),
-            NS = '.kendoQuiz',
-            STRING = 'string',
-            UNDEFINED = 'undefined',
-            CHANGE = 'change',
-            CLICK = 'click',
-            ACTIVE = 'k-state-active',
-            DISABLE = 'k-state-disabled',
-            WIDGET_CLASS = 'kj-quiz', // 'k-widget kj-quiz',
-            GROUP_CLASS = 'kj-quiz-group',
-            BUTTON = '<input type="button" class="k-button" value="{0}">',
-            RADIO = '<div><input id="{1}_{2}" name="{1}" type="radio" value="{0}"><label for="{1}_{2}">{0}</label></div>',
-            MARGIN = '0.2em',
-            MODES = {
+        var kendo = window.kendo;
+        var ui = kendo.ui;
+        var Widget = ui.Widget;
+        var assert = window.assert;
+        var logger = new window.Log('kidoju.widgets.quiz');
+        var NS = '.kendoQuiz';
+        var STRING = 'string';
+        var UNDEFINED = 'undefined';
+        var CHANGE = 'change';
+        var CLICK = 'click';
+        var ACTIVE = 'k-state-active';
+        var DISABLE = 'k-state-disabled';
+        var WIDGET_CLASS = 'kj-quiz'; // 'k-widget kj-quiz',
+        var GROUP_CLASS = 'kj-quiz-group';
+        var BUTTON = '<input type="button" class="k-button" value="{0}">';
+        var RADIO = '<div><input id="{1}_{2}" name="{1}" type="radio" value="{0}"><label for="{1}_{2}">{0}</label></div>';
+        var MARGIN = '0.2em';
+        var MODES = {
                 BUTTON: 'button',
                 DROPDOWN: 'dropdown',
                 RADIO: 'radio'
@@ -54,9 +54,9 @@
          */
         function randomString(length) {
             var s = new Array(length + 1).join('x');
-            return s.replace(/x/g, function(c) {
+            return s.replace(/x/g, function (c) {
                 /* jshint -W016 */
-                return (Math.random()*16|0).toString(16);
+                return (Math.random() * 16|0).toString(16);
                 /* jshint +W016 */
             });
         }
@@ -69,11 +69,13 @@
             if ($.isPlainObject(style)) {
                 return style;
             } else if ($.type(style) === STRING) {
-                var ret = {}, styleArray = style.split(';');
+                var ret = {};
+                var styleArray = style.split(';');
                 for (var i = 0; i < styleArray.length; i++) {
                     var styleKeyValue = styleArray[i].split(':');
                     if ($.isArray(styleKeyValue) && styleKeyValue.length === 2) {
-                        var key = styleKeyValue[0].trim(), value = styleKeyValue[1].trim();
+                        var key = styleKeyValue[0].trim();
+                        var value = styleKeyValue[1].trim();
                         if (key.length && value.length) {
                             ret[key] = value;
                         }
@@ -99,7 +101,7 @@
              * @param element
              * @param options
              */
-            init: function(element, options) {
+            init: function (element, options) {
                 var that = this;
                 Widget.fn.init.call(that, element, options);
                 logger.debug('widget initialized');
@@ -109,7 +111,7 @@
                 that._layout();
                 that._dataSource();
                 that._enable = true;
-                if(!that.options.enable) {
+                if (!that.options.enable) {
                     that._enable = false;
                     that.wrapper.addClass(DISABLE);
                 }
@@ -144,7 +146,7 @@
              *
              * @param options
              */
-            setOptions: function(options) {
+            setOptions: function (options) {
                 var that = this;
                 Widget.fn.setOptions.call(that, options);
                 options = that.options;
@@ -168,7 +170,7 @@
              * Gets/sets the value
              * @param value
              */
-            value: function(value) {
+            value: function (value) {
                 var that = this;
                 if ($.type(value) === STRING || value === null) {
                     that._value = value;
@@ -184,7 +186,7 @@
              * Widget layout
              * @private
              */
-            _layout: function() {
+            _layout: function () {
                 var that = this;
                 that.wrapper = that.element;
                 that.element.addClass(WIDGET_CLASS);
@@ -201,7 +203,7 @@
              * Widget layout as dropdown list
              * @private
              */
-            _layoutDropDown: function() {
+            _layoutDropDown: function () {
                 var that = this;
                 that.dropDownList = $('<input>')
                     .width('100%')
@@ -221,7 +223,7 @@
              * Event handler triggered when changing the value of the drop down list in the header
              * @private
              */
-            _onDropDownListChange: function() {
+            _onDropDownListChange: function () {
                 var that = this;
                 assert.instanceof(kendo.ui.DropDownList, that.dropDownList, kendo.format(assert.messages.instanceof.default, 'this.dropDownList', 'kendo.ui.DropDownList'));
                 var value = that.dropDownList.value();
@@ -237,7 +239,7 @@
              * Widget layout as buttons or radios
              * @private
              */
-            _layoutGroup: function() {
+            _layoutGroup: function () {
                 var that = this;
                 that.groupList = $('<div>')
                     .addClass(GROUP_CLASS)
@@ -252,11 +254,11 @@
              * @param e
              * @private
              */
-            _onClick: function(e) {
+            _onClick: function (e) {
                 assert.instanceof($.Event, e, kendo.format(assert.messages.instanceof.default, 'e', 'jQuery.Event'));
-                var that = this,
-                    target = $(e.target),
-                    value = target.val();
+                var that = this;
+                var target = $(e.target);
+                var value = target.val();
                 if (value !== that._value) {
                     that._value = value;
                 } else { // clicking the same value resets the button (and value)
@@ -270,9 +272,9 @@
              * Update UI when value is changed
              * @private
              */
-            _toggleUI: function() {
+            _toggleUI: function () {
                 var that = this;
-                switch(that.options.mode) {
+                switch (that.options.mode) {
                     case MODES.BUTTON:
                         assert.instanceof($, that.groupList, kendo.format(assert.messages.instanceof.default, 'this.groupList', 'jQuery'));
                         that.groupList.find('input[type=button]')
@@ -313,7 +315,7 @@
              * _dataSource function to bind refresh to the change event
              * @private
              */
-            _dataSource: function() {
+            _dataSource: function () {
                 var that = this;
 
                 // returns the datasource OR creates one if using array or configuration
@@ -328,7 +330,7 @@
 
                 // Assign dataSource to dropDownList
                 var dropDownList = that.dropDownList;
-                if (dropDownList instanceof kendo.ui.DropDownList && dropDownList.dataSource !== that.dataSource ) {
+                if (dropDownList instanceof kendo.ui.DropDownList && dropDownList.dataSource !== that.dataSource) {
                     dropDownList.setDataSource(that.dataSource);
                 }
 
@@ -342,7 +344,7 @@
              * sets the dataSource for source binding
              * @param dataSource
              */
-            setDataSource: function(dataSource) {
+            setDataSource: function (dataSource) {
                 var that = this;
                 // set the internal datasource equal to the one passed in by MVVM
                 that.options.dataSource = dataSource;
@@ -355,7 +357,7 @@
              * for example to add buttons or options
              * @param e
              */
-            refresh: function(e) {
+            refresh: function (e) {
                 var that = this;
                 if (that.options.mode === MODES.DROPDOWN) {
                     assert.instanceof(kendo.ui.DropDownList, that.dropDownList, kendo.format(assert.messages.instanceof.default, 'that.dropDownList', 'kendo.ui.DropDownList'));
@@ -367,7 +369,7 @@
                         items = e.items;
                     }
                     that.groupList.empty();
-                    $(e.items).each(function(index, value) {
+                    $(e.items).each(function (index, value) {
                         if (that.options.mode === MODES.BUTTON) {
                             $(kendo.format(BUTTON, value))
                                 .css(that.options.itemStyle)
@@ -377,12 +379,12 @@
                                 .css(that.options.itemStyle)
                                 .appendTo(that.groupList);
                             var size = parseInt(radio.css('fontSize'));
-                            if(!isNaN(size)){
+                            if (!isNaN(size)){
                                 // TODO See http://www.telerik.com/forums/font-size-of-styled-radio-buttons-and-checkboxes
                                 // TODO consider as part of resize event handler
                                 radio.find('input[type=radio]')
-                                    .height(0.6*size)
-                                    .width(0.6*size);
+                                    .height(0.6 * size)
+                                    .width(0.6 * size);
                             }
                         }
                     });
@@ -393,14 +395,14 @@
              * Enable/disable the widget
              * @param enable
              */
-            enable: function(enable) {
+            enable: function (enable) {
                 var wrapper = this.wrapper;
 
-                if(typeof enable === UNDEFINED) {
+                if (typeof enable === UNDEFINED) {
                     enable = true;
                 }
 
-                if(enable) {
+                if (enable) {
                     wrapper.removeClass(DISABLE);
                 } else {
                     wrapper.addClass(DISABLE);
@@ -413,7 +415,7 @@
              * Clear layout
              * @private
              */
-            _clear: function() {
+            _clear: function () {
                 var that = this;
                 // unbind kendo
                 kendo.unbind($(that.element));
@@ -428,7 +430,7 @@
             /**
              * Destroy widget
              */
-            destroy: function() {
+            destroy: function () {
                 var that = this;
                 Widget.fn.destroy.call(that);
                 that._clear();
@@ -443,4 +445,4 @@
 
     return window.kendo;
 
-}, typeof define === 'function' && define.amd ? define : function(_, f){ 'use strict'; f(); });
+}, typeof define === 'function' && define.amd ? define : function (_, f) { 'use strict'; f(); });

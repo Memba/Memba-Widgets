6196f9ab7b787490a7b02e072622487268fcc8df
jlchereau <jlchereau@msn.com>
2015-09-27 13:02:10 +0100

    Fixed jscs and jshint errors

--- src/js/window.assert.js
+++ src/js/window.assert.js
@@ -6,7 +6,7 @@
 /* jshint browser: true, jquery: true */
 /* globals define: false */
 
-(function (f, define){
+(function (f, define) {
     'use strict';
     define([], f);
 })(function () {
@@ -15,8 +15,8 @@
 
     (function ($, undefined) {
 
-        var STRING = 'string',
-            UNDEFINED = 'undefined';
+        var STRING = 'string';
+        var UNDEFINED = 'undefined';
 
         /**
          * Asserts
@@ -25,19 +25,19 @@
          */
         var assert = window.assert = $.extend(
             // By extending assert, we ensure we can call both assert() and assert.ok() for the same result (like in nodeJS)
-            function(test, message) {
+            function (test, message) {
                 if (!test) { throw new Error(message); }
             },
             {
-                enum: function(array, value, message) { if (array.indexOf(value) === -1) { throw new Error(message); } },
-                equal: function(expected, actual, message) { if (expected !== actual) { throw new Error(message); } },
-                instanceof: function(Class, value, message) { if (!(value instanceof Class)) { throw new Error(message); } },
-                isOptionalObject: function(value, message) { if ($.type(value) !== UNDEFINED && (!$.isPlainObject(value) || $.isEmptyObject(value))) { throw new Error(message); } },
-                isPlainObject: function(value, message) { if (!$.isPlainObject(value) || $.isEmptyObject(value)) { throw new Error(message); } },
-                isUndefined: function(value, message) { if ($.type(value) !== UNDEFINED) { throw new Error(message); } },
-                match: function(rx, value, message) { if ($.type(value) !== STRING || !rx.test(value)) { throw new Error(message); } },
-                ok: function(test, message) { return assert(test, message); },
-                type: function(type, value, message) { if ($.type(value) !== type) { throw new TypeError(message); } }
+                enum: function (array, value, message) { if (array.indexOf(value) === -1) { throw new Error(message); } },
+                equal: function (expected, actual, message) { if (expected !== actual) { throw new Error(message); } },
+                instanceof: function (Class, value, message) { if (!(value instanceof Class)) { throw new Error(message); } },
+                isOptionalObject: function (value, message) { if ($.type(value) !== UNDEFINED && (!$.isPlainObject(value) || $.isEmptyObject(value))) { throw new Error(message); } },
+                isPlainObject: function (value, message) { if (!$.isPlainObject(value) || $.isEmptyObject(value)) { throw new Error(message); } },
+                isUndefined: function (value, message) { if ($.type(value) !== UNDEFINED) { throw new Error(message); } },
+                match: function (rx, value, message) { if ($.type(value) !== STRING || !rx.test(value)) { throw new Error(message); } },
+                ok: function (test, message) { return assert(test, message); },
+                type: function (type, value, message) { if ($.type(value) !== type) { throw new TypeError(message); } }
             },
             {
                 messages: {
@@ -70,4 +70,4 @@
 
     return window.kendo;
 
-}, typeof define === 'function' && define.amd ? define : function (_, f){ 'use strict'; f(); });
+}, typeof define === 'function' && define.amd ? define : function (_, f) { 'use strict'; f(); });

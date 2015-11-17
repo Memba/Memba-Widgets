8461e88074010067ab54156d861efd73cfe5c829
jlchereau <jlchereau@msn.com>
2015-11-17 10:22:33 +0000

    Time to commit before travelling (unstable)

--- src/js/window.log.js
+++ src/js/window.log.js
@@ -13,10 +13,28 @@
 
     'use strict';
 
-    (function ($, undefined) {
+    (function (undefined) {
+
+        var app = window.app || {};
+        var appLogger = app.logger;
 
         var STRING = 'string';
-        var app = window.app;
+        var FUNCTION = 'function';
+        var UNDEFINED = 'undefined';
+        var LEVELS = {
+            // See https://github.com/logentries/le_node#log-levels
+            DEBUG: { NAME: 'DEBUG', VALUE: 1 },
+            INFO: { NAME: 'INFO', VALUE: 2 },
+            WARN: { NAME: 'WARN', VALUE: 4 },
+            ERROR: { NAME: 'ERROR', VALUE: 5 },
+            CRIT: { NAME: 'CRIT', VALUE: 6 }
+        };
+        var DEFAULT = LEVELS.INFO;
+        var LINEFEEDS = /\n/g;
+        var LINESEP = '; ';
+        var EQUAL = ' = ';
+        var FIRST = '  ';
+        var SEPARATOR = '  |  ';
 
         /**
          * Logger class
@@ -27,93 +45,234 @@
             this._module = module;
 
             /**
-             * Process a log entry
-             * @param entry
+             * Preprocess message + data
+             * @param message
+             * @param data
              */
-            function process(entry) {
+            function preProcess(message, data) {
+                if (typeof message !== STRING && typeof data !== UNDEFINED) {
+                    throw new TypeError('Unexpected data when message is not a string');
+                }
+                var logEntry;
+                if (typeof message === STRING) {
+                    logEntry = { message: message, data: data };
+                } else if (message instanceof window.Error) {
+                    logEntry = {
+                        message: error.message,
+                        error: message
+                    };
+                } else if (message instanceof window.ErrorEvent) {
+                    logEntry = {
+                        message: message.message,
+                        data: { filename: message.filename, lineno: message.lineno, colno: message.colno },
+                        error: message.error
+                    };
+                } else if (Object.prototype.toString.call(message) === '[object Object]') {
+                    logEntry = JSON.parse(JSON.stringify(message));
+                    if (message.error instanceof Error) {
+                        // We need to do that because JSON.stringify(new Error('Oops)) === {}
+                        logEntry.error = message.error;
+                    }
+                } else {
+                    logEntry = { data: message };
+                }
+                return logEntry;
+            }
 
+            /**
+             * Enhance a log entry
+             * @param logEntry
+             * @param module
+             * @param level
+             */
+            function enhance(logEntry, module, level) {
+                if (Object.prototype.toString.call(logEntry) !== '[object Object]') {
+                    throw new TypeError('logEntry is expected to be an object');
+                }
+
+                // Improve error logging
+                if (logEntry.error instanceof Error) {
+                    if (typeof logEntry.message === UNDEFINED) {
+                        logEntry.message = logEntry.error.message;
+                    }
+                    if (logEntry.error.originalError instanceof window.Error) {
+                        logEntry.original = logEntry.error.originalError.message;
+                        logEntry.stack = logEntry.error.originalError.stack.toString().replace(LINEFEEDS, LINESEP);
+                    } else {
+                        logEntry.stack = logEntry.error.stack.toString().replace(LINEFEEDS, LINESEP);
+                    }
+                }
+
+                // Add module
+                logEntry.module = typeof module === STRING ? module : UNDEFINED;
+
+                // Add level
+                level = String(level).toUpperCase();
+                logEntry.level = Object.keys(LEVELS).indexOf(level) > -1 ? level : DEFAULT.NAME;
+
+                // If there is a hidden input field named `trace` on the page, read it
+                var input = document.getElementById('trace');
+                if (input instanceof HTMLInputElement) {
+                    logEntry.trace = input.value;
+                }
             }
 
             /* This function's cyclomatic complexity is too high. */
             /* jshint -W074 */
 
             /**
-             * Generic print function
-             * @param message
-             * @param level
+             * Print a formatted log entry to the console
+             * @param logEntry
              * @private
              */
-            this._print = function (message, level) {
-                var error;
-                var logEntry;
-                if ($.type(message) === STRING) {
-                    logEntry = { message: message };
-                } else if (message instanceof Error) {
-                    error = message;
-                    logEntry = {
-                        message: error.message,
-                        data: $.extend({}, error)
-                    };
-                } else {
-                    logEntry = $.extend({}, message);
-                }
-                logEntry.level = (/^(debug|info|warn|error|crit)$/i).test(level) ? level.toUpperCase() : 'INFO';
-                logEntry.module = this._module;
-
-                // If we have an app.logger, delegate to app logger
-                if (app && app.logger) {
-                    $.noop(); // TODO <----------------------------------------------------------------
-                // Otherwise use plain old window.console.log
-                } else if (window.console && $.isFunction(window.console.log)) {
-                    window.console.log(
-                        '[' + logEntry.level + '] ' + (logEntry.level.length === 4 ? ' ' : '') +
-                        logEntry.module + ': ' +
-                        logEntry.message +
-                        (logEntry.data ? ' - ' + JSON.stringify(logEntry.data) : '')
-                    );
-                    if (error instanceof Error && $.isFunction(window.console.error)) {
-                        window.console.error(error);
+            function print(logEntry) {
+                if (app.DEBUG && window.console && typeof window.console.log === FUNCTION) {
+                    var message = label;
+                    var first = true;
+                    if (logEntry.message) {
+                        message += (first ? FIRST : SEPARATOR) + 'message' + EQUAL + logEntry.message;
+                        first = false;
+                    }
+                    if (logEntry.original) {
+                        message += (first ? FIRST : SEPARATOR) + 'original' + EQUAL + logEntry.original;
+                        first = false;
+                    }
+                    if (logEntry.module) {
+                        message += (first ? FIRST : SEPARATOR) + 'module' + EQUAL + logEntry.module;
+                        first = false;
+                    }
+                    if (logEntry.method) {
+                        message += (first ? FIRST : SEPARATOR) + 'method' + EQUAL + logEntry.method;
+                        first = false;
+                    }
+                    if (logEntry.stack) {
+                        message += (first ? FIRST : SEPARATOR) + 'stack' + EQUAL + logEntry.stack;
+                        first = false;
+                    }
+                    if (logEntry.data) {
+                        try {
+                            message += (first ? FIRST : SEPARATOR) + 'data' + EQUAL + JSON.stringify(logEntry.data);
+                        } catch (exception) {
+                            if (typeof logEntry.data.toString === FUNCTION) {
+                                message += (first ? FIRST : SEPARATOR) + 'data' + EQUAL + logEntry.data.toString();
+                            }
+                        }
+                    }
+                    if (logEntry.trace) {
+                        message += (first ? FIRST : SEPARATOR) + 'trace' + EQUAL + logEntry.trace;
+                        first = false;
+                    }
+                    window.console.log(message);
+                    if (logEntry.error instanceof Error) {
+                        if (typeof window.console.error === FUNCTION) {
+                            window.console.error(logEntry.error);
+                        } else if (typeof window.console.dir === FUNCTION) {
+                            window.console.dir(logEntry.error);
+                        }
+                    }
+                    if (logEntry.originalError instanceof Error) {
+                        if (typeof window.console.error === FUNCTION) {
+                            window.console.error(logEntry.originalError);
+                        } else if (typeof window.console.dir === FUNCTION) {
+                            window.console.dir(logEntry.originalError);
+                        }
                     }
                 }
-            };
+            }
 
             /* jshint +W074 */
+
+            /**
+             * Debug message
+             * @param message
+             * @param data
+             */
+            this.debug = function (message, data) {
+                var logEntry = preProcess(message, data);
+                enhance(logEntry, this._module, LEVELS.DEBUG.NAME);
+                print(logEntry, LEVELS.DEBUG.NAME);
+                if (appLogger && typeof appLogger.debug === FUNCTION) {
+                    appLogger.debug(logEntry);
+                }
+                return true;
+            };
+
+            /**
+             * Info message
+             * @param message
+             * @param data
+             */
+            this.info = function (message, data) {
+                var logEntry = preProcess(message, data);
+                enhance(logEntry, this._module, LEVELS.INFO.NAME);
+                print(logEntry, LEVELS.INFO.NAME);
+                if (appLogger && typeof appLogger.debug === FUNCTION) {
+                    appLogger.debug(logEntry);
+                }
+                return true;
+            };
+
+            /**
+             * Warning message
+             * @param message
+             * @param data
+             */
+            this.warn = function (message, data) {
+                var logEntry = preProcess(message, data);
+                enhance(logEntry, this._module, LEVELS.WARN.NAME);
+                print(logEntry, LEVELS.WARN.NAME);
+                if (appLogger && typeof appLogger.debug === FUNCTION) {
+                    appLogger.debug(logEntry);
+                }
+                return true;
+            };
+
+            /**
+             * Error message
+             * @param message
+             * @param data
+             */
+            this.error = function (message, data) {
+                var logEntry = preProcess(message, data);
+                enhance(logEntry, this._module, LEVELS.ERROR.NAME);
+                print(logEntry, LEVELS.ERROR.NAME);
+                if (appLogger && typeof appLogger.debug === FUNCTION) {
+                    appLogger.debug(logEntry);
+                }
+                // TODO Display error
+                return true;
+            };
+
+            /**
+             * Critical message
+             * @param message
+             * @param data
+             */
+            this.critical = function (message, data) {
+                var logEntry = preProcess(message, data);
+                enhance(logEntry, this._module, LEVELS.CRIT.NAME);
+                print(logEntry, LEVELS.CRIT.NAME);
+                if (appLogger && typeof appLogger.debug === FUNCTION) {
+                    appLogger.debug(logEntry);
+                }
+                // TODO Display error
+                return true;
+            };
 
         };
 
         /**
-         * Print debug message
-         * @param message
+         * OnError global event handler
+         * @see https://developer.mozilla.org/en-US/docs/Web/API/GlobalEventHandlers/onerror
+         * @param e
          */
-        Logger.prototype.debug = function (message) { return this._print(message, 'debug'); };
+        window.onerror = function (e) {
+            var logger = new Logger('global');
+            logger.critical(e);
+        };
 
-        /**
-         * Print info message
-         * @param message
-         */
-        Logger.prototype.info = function (message) { return this._print(message, 'info'); };
+    }());
 
-        /**
-         * Print warn message
-         * @param message
-         */
-        Logger.prototype.warn = function (message) { return this._print(message, 'warn'); };
-
-        /**
-         * Print error message
-         * @param message
-         */
-        Logger.prototype.error = function (message) { return this._print(message, 'error'); };
-
-        /**
-         * Print critical message
-         * @param message
-         */
-        Logger.prototype.crit = function (message) { return this._print(message, 'crit'); };
-
-
-    }(window.jQuery));
-
-    return window.kendo;
+    return window.Logger;
 
 }, typeof define === 'function' && define.amd ? define : function (_, f) { 'use strict'; f(); });
--- test/browsers/window.log.test.js
+++ test/browsers/window.log.test.js
@@ -1,3 +1,92 @@
 /**
- * Created by jlchereau on 23/09/2015.
+ * Copyright (c) 2013-2015 Memba Sarl. All rights reserved.
+ * Sources at https://github.com/Memba
  */
+
+/* jshint browser: true, jquery: true, mocha: true, expr: true */
+
+;(function (window, $, undefined) {
+
+    'use strict';
+
+    var expect = window.chai.expect;
+    var sinon = window.sinon;
+    // var kendo = window.kendo;
+    // var FIXTURES = '#fixtures';
+    var assert = window.assert;
+
+    describe('window.log.test', function () {
+
+        /*
+        before(function () {
+           if (window.__karma__ && $(FIXTURES).length === 0) {
+               $('body').append('<div id="fixtures"></div>');
+           }
+        });
+        */
+
+        describe('logging without app.logger', function () {
+
+            it('debug', function () {
+                function fn() {
+                    assert(false, ERR_MSG);
+                }
+                expect(fn).to.throw(Error, ERR_MSG);
+                expect(assert(true, ERR_MSG)).to.be.undefined;
+            });
+
+            it('info', function () {
+
+            });
+
+            it('warn', function () {
+
+            });
+
+            it('error', function () {
+
+            });
+
+            it('crit', function () {
+
+            });
+
+        });
+
+        describe('logging with app.logger', function () {
+
+            var app = {
+                logger: {
+                    debug: sinon.spy(),
+                    info: sinon.spy(),
+                    warn: sinon.spy(),
+                    error: sinon.spy(),
+                    crit: sinon.spy()
+                }
+            };
+
+            it('debug', function () {
+
+            });
+
+            it('info', function () {
+
+            });
+
+            it('warn', function () {
+
+            });
+
+            it('error', function () {
+
+            });
+
+            it('crit', function () {
+
+            });
+
+        });
+
+    });
+
+}(this, jQuery));

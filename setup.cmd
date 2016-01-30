311676af095f4885193755dfe38f1ee205892e56
jlchereau <jlchereau@msn.com>
2016-01-30 11:53:54 +0000

    Updated setup

--- setup.cmd
+++ setup.cmd
@@ -7,8 +7,11 @@ REM Install global packages
 call npm update -g npm
 call npm install -g grunt-cli
 call npm install -g istanbul
-call npm install -g mocha
 call npm install -g karma-cli
+REM call npm install -g mocha
+call npm install -g nodemon
+call npm install -g webpack
+call npm install -g webpack-dev-server
 REM -------------------------
 REM Install dependencies listed in package.json
 call npm install

2e6604fae01bf4b6aee8ed98fdf54ee433a9cafb
jlchereau <jlchereau@msn.com>
2016-02-26 08:25:42 +0000

    Added placeholder for markdown tests

--- test/browsers/kidoju.widgets.markdown.test.js
+++ test/browsers/kidoju.widgets.markdown.test.js
@@ -17,15 +17,7 @@
     var CHANGE = 'change';
     var FIXTURES = '#fixtures';
     var MARKDOWN1 = '<div id="markdown1"></div>';
-    var MARKDOWN2 = '<div id="markdown2" data-role="mediaplayer"></div>';
-
-    /**
-     * HTMLMediaElement is not supported in PhantomJS
-     * @see https://github.com/ariya/phantomjs/issues/10839
-     */
-    if (window.PHANTOMJS) {
-        return;
-    }
+    var MARKDOWN2 = '<div id="markdown2" data-role="markdown"></div>';
 
     describe('kidoju.widgets.markdown', function () {
 

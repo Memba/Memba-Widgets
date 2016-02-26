e2eb450e5d700537ae1f668734d5f02fb687dd96
jlchereau <jlchereau@msn.com>
2016-02-26 08:25:56 +0000

    Added placeholder for dropzone tests

---
+++ test/browsers/kidoju.widgets.dropzone.test.js
@@ -0,0 +1,27 @@
+/**
+ * Copyright (c) 2013-2015 Memba Sarl. All rights reserved.
+ * Sources at https://github.com/Memba
+ */
+
+/* jshint browser: true, jquery: true, mocha: true, expr: true */
+
+;(function (window, $, undefined) {
+
+    'use strict';
+
+    var expect = window.chai.expect;
+    var sinon = window.sinon;
+    var kendo = window.kendo;
+    var ui = kendo.ui;
+    var DropZone = ui.DropZone;
+    var FIXTURES = '#fixtures';
+    var DROPZONE1 = '<div id="dropzone1"></div>';
+    var DROPZONE2 = '<div id="dropzone2" data-role="dropzone"></div>';
+
+    describe('kidoju.widgets.dropzone', function () {
+
+        // TODO
+
+    });
+
+}(this, jQuery));

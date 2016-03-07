61470d6eea1a735c5c8ad825debe746633805f29
jlchereau <jlchereau@msn.com>
2016-03-07 14:52:40 +0000

    Placeholder for Connector tests

---
+++ test/browsers/kidoju.widgets.connector.test.js
@@ -0,0 +1,27 @@
+/**
+ * Copyright (c) 2013-2015 Memba Sarl. All rights reserved.
+ * Sources at https://github.com/Memba
+ */
+
+/* jshint browser: true, jquery: true, mocha: true, expr: true */
+
+;(function (window, $, undefined) {
+
+    'use strict';
+
+    var expect = window.chai.expect;
+    var sinon = window.sinon;
+    var kendo = window.kendo;
+    var ui = kendo.ui;
+    var CharGrid = ui.Connector;
+    var FIXTURES = '#fixtures';
+    var CONNECTOR1 = '<div id="connector1"></div>';
+    var CONNECTOR2 = '<div id="connector2" data-role="connector"></div>';
+
+    describe('kidoju.widgets.connector', function () {
+
+        // TODO
+
+    });
+
+}(this, jQuery));

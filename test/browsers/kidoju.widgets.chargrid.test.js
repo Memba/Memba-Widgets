e36a2e095d8a0d1c57efdc4f5798e2b344fc6de9
jlchereau <jlchereau@msn.com>
2016-03-07 14:48:59 +0000

    New placeholder for chargrid tests

---
+++ test/browsers/kidoju.widgets.chargrid.test.js
@@ -0,0 +1,27 @@
+/**
+ * Copyright (c) 2013-2015 Memba Sarl. All rights reserved.
+ * Sources at https://github.com/Memba
+ */
+
+/* jshint browser: true, jquery: true, mocha: true, expr: true */
+
+;(function (window, $, undefined) {
+
+    'use strict';
+
+    var expect = window.chai.expect;
+    var sinon = window.sinon;
+    var kendo = window.kendo;
+    var ui = kendo.ui;
+    var CharGrid = ui.CharGrid;
+    var FIXTURES = '#fixtures';
+    var DROPZONE1 = '<div id="chargrid1"></div>';
+    var DROPZONE2 = '<div id="chargrid2" data-role="chargrid"></div>';
+
+    describe('kidoju.widgets.chargrid', function () {
+
+        // TODO
+
+    });
+
+}(this, jQuery));

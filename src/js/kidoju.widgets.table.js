b7216056ce670ef596b13e61dacf59aef5bba502
jlchereau <jlchereau@msn.com>
2016-09-12 21:02:10 +0100

    Added new table widget https://github.com/kidoju/Kidoju-Widgets/issues/122

---
+++ src/js/kidoju.widgets.table.js
@@ -0,0 +1,247 @@
+/**
+ * Copyright (c) 2013-2016 Memba Sarl. All rights reserved.
+ * Sources at https://github.com/Memba
+ */
+
+/* jshint browser: true, jquery: true */
+/* globals define: false */
+
+(function (f, define) {
+    'use strict';
+    define([
+        './window.assert',
+        './window.logger',
+        './vendor/kendo/kendo.binder'
+        // './vendor/kendo/kendo.multiselect' // required because of a test in kendo.binder.js
+    ], f);
+})(function () {
+
+    'use strict';
+
+    /* This function has too many statements. */
+    /* jshint -W071 */
+
+    (function ($, undefined) {
+
+        var kendo = window.kendo;
+        var Widget = kendo.ui.Widget;
+        var assert = window.assert;
+        var logger = new window.Logger('kidoju.widgets.table');
+        // var NUMBER = 'number';
+        var OBJECT = 'object';
+        var NULL = 'null';
+        var UNDEFINED = 'undefined';
+        var CHANGE = 'change';
+        var DOT = '.';
+        var WIDGET = 'kendoTable';
+        var NS = DOT + WIDGET;
+        var TABLE = '<table/>';
+        var WIDGET_CLASS = 'kj-table';
+        // var ATTRIBUTE_SELECTOR = '[{0}="{1}"]';
+        // var ID = 'id';
+
+        /*********************************************************************************
+         * Widget
+         *********************************************************************************/
+
+        /**
+         * Table
+         * @class Table Widget (kendoConnector)
+         */
+        var Table = Widget.extend({
+
+            /**
+             * Initializes the widget
+             * @param element
+             * @param options
+             */
+            init: function (element, options) {
+                var that = this;
+                options = options || {};
+                Widget.fn.init.call(that, element, options);
+                logger.debug('widget initialized');
+                that._layout();
+                that._enabled = that.element.prop('disabled') ? false : that.options.enable;
+                kendo.notify(that);
+            },
+
+            /**
+             * Widget options
+             * @property options
+             */
+            options: {
+                name: 'Table',
+                columns: 5,
+                rows: 8,
+                enable: true
+            },
+
+            // events: [
+            //    CHANGE
+            // ],
+
+            /**
+             * Value for MVVM binding
+             * @param value
+             */
+            value: function (value) {
+                var that = this;
+                if ($.type(value) === OBJECT || $.type(value) === NULL) {
+                    that._value = that._parse(value);
+                    that.render();
+                } else if ($.type(value) === UNDEFINED) {
+                    return that._value;
+                } else {
+                    throw new TypeError('`value` is expected to be a nullable object if not undefined');
+                }
+            },
+
+            /**
+             * Widget layout
+             * @private
+             */
+            _layout: function () {
+                // if (!(this.element[0] instanceof HTMLTableElement)) {
+                //    throw new Error('Please use a <table/> element to initialize a kendo.ui.Table');
+                // }
+                this.wrapper = this.element;
+                this.element
+                    .addClass(WIDGET_CLASS)
+                    .css({
+                        position: 'relative'
+                        // tableLayout: 'fixed',
+                        // borderCollapse: 'collapse',
+                        // borderSpacing: 0
+                        // display: 'table'
+                    });
+            },
+
+            /**
+             * Parse Kendo UI Spreadsheet JSON
+             * @param value
+             * @private
+             */
+            _parse: function (value) {
+                var ret = [];
+                var rowTotal = this.options.rows;
+                var columnTotal = this.options.columns;
+                var columnDefinitions = value && value.sheets && value.sheets.length && value.sheets[0].columns || [];
+                var rowDefinitions = value && value.sheets && value.sheets.length && value.sheets[0].rows || [];
+                var height;
+                var left = 0;
+                var top = 0;
+                var width;
+                for (var i = 0; i < rowTotal; i++) {
+                    ret.push([]);
+                    var row = ret[i];
+                    var rowDefinition = rowDefinitions.find(function (item) { return item.index === i }) || { cells: [] };
+                    for (var j = 0; j < columnTotal; j++) {
+                        var cellDefinition = rowDefinition.cells.find(function (item) { return item.index === j });
+                        var columnDefinition = columnDefinitions.find(function (item) { return item.index === j });
+                        height = rowDefinition && rowDefinition.height || 19;
+                        width = columnDefinition && columnDefinition.width || 63;
+                        if ($.type(cellDefinition) === UNDEFINED) {
+                            row.push({
+                                css: {
+                                    left: left,
+                                    height: height,
+                                    top: top,
+                                    width: width
+                                }
+                            });
+                        } else {
+                            row.push({
+                                value: cellDefinition.value,
+                                format: cellDefinition.format,
+                                css: {
+                                    backgroundColor: cellDefinition.background,
+                                    borderBottom: cellDefinition.borderBottom ? 'solid ' + cellDefinition.borderBottom.size + 'px ' + cellDefinition.borderBottom.color : undefined,
+                                    borderLeft: cellDefinition.borderLeft ? 'solid ' + cellDefinition.borderLeft.size + 'px ' + cellDefinition.borderLeft.color : undefined,
+                                    borderRight: cellDefinition.borderRight ? 'solid ' + cellDefinition.borderRight.size + 'px ' + cellDefinition.borderRight.color : undefined,
+                                    borderTop: cellDefinition.borderTop ? 'solid ' + cellDefinition.borderTop.size + 'px ' + cellDefinition.borderTop.color : undefined,
+                                    color: cellDefinition.color,
+                                    fontFamily: cellDefinition.fontFamily || 'Arial',
+                                    fontSize: cellDefinition.fontSize || 12,
+                                    fontStyle: cellDefinition.italic ? 'italic' : 'normal',
+                                    fontWeight: cellDefinition.bold ? 'bold' : 'normal',
+                                    left: left,
+                                    height: height,
+                                    textAlign: cellDefinition.textAlign || (isNaN(parseFloat(cellDefinition.value)) ? 'left' : 'right'), // TODO: Dates parse as numbers but are left aligned by default
+                                    textDecoration: cellDefinition.underline ? 'underline' : 'none',
+                                    top: top,
+                                    // verticalAlign: cellDefinition.verticalAlign === 'center' ? 'middle' : (cellDefinition.verticalAlign || 'bottom'),
+                                    width: width,
+                                    whiteSpace: cellDefinition.wrap ? 'pre-wrap' : undefined,
+                                    wordBreak: cellDefinition.wrap ? 'break-all' : undefined
+                                },
+                                class: 'k-vertical-align-' + (cellDefinition.verticalAlign || 'bottom')
+                            });
+                        }
+                        left += width;
+                    }
+                    left = 0;
+                    top += height;
+                }
+                return ret;
+            },
+
+            /**
+             * Widget rendering
+             */
+            render: function () {
+                var element = this.element;
+                var rowTotal = this.options.rows;
+                var columnTotal = this.options.columns;
+                element.empty();
+                for (var i = 0; i < rowTotal; i++) {
+                    for (var j = 0; j < columnTotal; j++) {
+                        var cell = this._value[i][j];
+                        var cellElement = $('<DIV/>')
+                            .addClass('k-spreadsheet-cell')
+                            .css(cell.css);
+                        var cellContent = '<DIV class="' + cell.class + '">';
+                        if ($.type(cell.value) === UNDEFINED) {
+                            cellContent += '&nbsp';
+                        } else if (!isNaN(parseFloat(cell.value))) { // TODO dates
+                            cellContent += '<span>' + kendo.toString(cell.value, cell.format) + '</span>';
+                        } else {
+                            cellContent += cell.value;
+                        }
+                        cellContent += '</DIV>';
+                        cellElement
+                            .html(cellContent)
+                            .appendTo(element);
+                    }
+                }
+            },
+
+            /**
+             * Destroys the widget including all DOM modifications
+             * @method destroy
+             */
+            destroy: function () {
+                var that = this;
+                var element = that.element;
+                Widget.fn.destroy.call(that);
+                // unbind and destroy all descendants
+                kendo.unbind(element);
+                kendo.destroy(element);
+                // unbind all other events (probably redundant)
+                element.find('*').off();
+                element.off();
+                // remove descendants
+                element.empty();
+                // remove widget class
+                element.removeClass(WIDGET_CLASS);
+            }
+        });
+
+        kendo.ui.plugin(Table);
+
+    }(window.jQuery));
+
+    /* jshint +W071 */
+
+    return window.kendo;
+
+}, typeof define === 'function' && define.amd ? define : function (_, f) { 'use strict'; f(); });

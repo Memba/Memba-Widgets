705d3472f521063fcc140d05cf9318922c501d74
jlchereau <jlchereau@msn.com>
2017-02-21 15:32:19 +0000

    Expanded copyright to 2017.

--- src/js/kidoju.widgets.listeditor.js
+++ src/js/kidoju.widgets.listeditor.js
@@ -1,1 +1,63 @@
+/**
+ * Copyright (c) 2013-2017 Memba Sarl. All rights reserved.
+ * Sources at https://github.com/Memba
+ */
 
+/* jshint browser: true, jquery: true */
+/* globals define: false */
+
+(function (f, define) {
+    'use strict';
+    define([
+        './window.assert',
+        './window.logger',
+        './vendor/kendo/kendo.binder'
+    ], f);
+})(function () {
+
+    'use strict';
+
+    (function ($, undefined) {
+
+        var kendo = window.kendo;
+        var Widget = kendo.ui.Widget;
+
+        /*********************************************************************************
+         * Widget
+         *********************************************************************************/
+
+        /**
+         * ListEditor
+         * @class ListEditor Widget (kendoListEditor)
+         */
+        var ListEditor = Widget.extend({
+
+            /**
+             * Initializes the widget
+             * @param element
+             * @param options
+             */
+            init: function (element, options) {
+                var that = this;
+                options = options || {};
+                Widget.fn.init.call(that, element, options);
+                logger.debug({ method: 'init', message: 'widget initialized' });
+                // kendo.notify(that);
+            },
+
+            /**
+             * Options
+             */
+            options: {
+                name: 'ListEditor'
+            }
+
+        });
+
+        kendo.ui.plugin(ListEditor);
+
+    }(window.jQuery));
+
+    return window.kendo;
+
+}, typeof define === 'function' && define.amd ? define : function (_, f) { 'use strict'; f(); });

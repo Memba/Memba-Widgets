789f4c0693d9bb600a63700c465aae9dd837c802
jlchereau <jlchereau@msn.com>
2017-03-08 13:25:18 +0000

    Logged refresh

--- src/js/kidoju.widgets.connector.js
+++ src/js/kidoju.widgets.connector.js
@@ -532,6 +532,7 @@
                 var container = that.element.closest(options.container);
                 assert.instanceof($, container, kendo.format(assert.messages.instanceof.default, 'container', 'jQuery'));
                 assert.instanceof(DataSource, that.dataSource, kendo.format(assert.messages.instanceof.default, 'this.dataSource', 'kendo.data.DataSource'));
+
                 var surface = container.data(SURFACE);
                 if (surface instanceof kendo.drawing.Surface) {
                     // Clear surface
@@ -561,6 +562,8 @@
                         }
                     });
                 }
+
+                logger.debug({ method: 'refresh', message: 'widget refreshed' });
             },
 
             /**

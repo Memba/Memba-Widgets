712f34cecbb0922fa394871fd62614f1fef977e1
jlchereau <jlchereau@msn.com>
2017-03-09 06:30:59 +0000

    Developed tests for workerlib https://github.com/kidoju/Kidoju-Widgets/issues/86

--- test/browsers/kidoju.data.workerlib.test.js
+++ test/browsers/kidoju.data.workerlib.test.js
@@ -3,25 +3,123 @@
  * Sources at https://github.com/Memba
  */
 
-/* jshint browser: true, jquery: true, mocha: true, expr: true */
+/* jshint browser: true, mocha: true, expr: true */
 
-;(function (window, $, undefined) {
+;(function (window, undefined) {
 
     'use strict';
 
     var expect = window.chai.expect;
     var sinon = window.sinon;
-    var kendo = window.kendo;
-    var ui = kendo.ui;
-    var DropDownTree = ui.DropDownTree;
-    var FIXTURES = '#fixtures';
-    var DROPDOWNTREE1 = '<div id="dropdowntree1"></div>';
-    var DROPDOWNTREE2 = '<div id="dropdowntree2" data-role="dropdowntree"></div>';
 
-    describe('kidoju.widgets.dropdowntree', function () {
+    describe('kidoju.data.workerlib.test', function () {
 
-        // TODO
+        it('Blacklisted globals', function () {
+            expect(window.ActiveXObject).to.be.undefined;
+            expect(window.clearInterval).to.be.undefined;
+            expect(window.clearTimeout).to.be.undefined;
+            expect(window.eval).to.be.undefined;
+            expect(window.Function).to.be.undefined;
+            expect(window.importScripts).to.be.undefined;
+            if(window.indexedDB) {
+                expect(window.indexedDB.open).to.be.undefined;
+                expect(window.indexedDB.deleteDatabase).to.be.undefined;
+                expect(window.indexedDB.cmp).to.be.undefined;
+            }
+            if (window.mozIndexedDB) {
+                expect(window.mozIndexedDB.open).to.be.undefined;
+                expect(window.mozIndexedDB.deleteDatabase).to.be.undefined;
+                expect(window.mozIndexedDB.cmp).to.be.undefined;
+            }
+            if (window.msIndexedDB) {
+                expect(window.msIndexedDB.open).to.be.undefined;
+                expect(window.msIndexedDB.deleteDatabase).to.be.undefined;
+                expect(window.msIndexedDB.cmp).to.be.undefined;
+            }
+            expect(window.requestFileSystem).to.be.undefined;
+            expect(window.requestFileSystemSync).to.be.undefined;
+            expect(window.resolveLocalFileSystemURL).to.be.undefined;
+            expect(window.resolveLocalFileSystemSyncURL).to.be.undefined;
+            expect(window.setInterval).to.be.undefined;
+            expect(window.setTimeout).to.be.undefined;
+            expect(window.XMLHttpRequest).to.be.undefined;
+            if (window.webkitIndexedDB) {
+                expect(window.webkitIndexedDB.open).to.be.undefined;
+                expect(window.webkitIndexedDB.deleteDatabase).to.be.undefined;
+                expect(window.webkitIndexedDB.cmp).to.be.undefined;
+            }
+            expect(window.webkitRequestFileSystem).to.be.undefined;
+            expect(window.webkitRequestFileSystemSync).to.be.undefined;
+            expect(window.webkitResolveLocalFileSystemURL).to.be.undefined;
+            expect(window.webkitResolveLocalFileSystemSyncURL).to.be.undefined;
+            expect(window.Worker).to.be.undefined;
+        });
+
+        it('Soundex', function () {
+            var SOUNDEX = [
+                { name: 'Soundex', value: 'S532' },
+                { name: 'Example', value: 'E251' },
+                { name: 'Sownteks', value: 'S532' },
+                { name: 'Ekzampul', value: 'E251' },
+                { name: 'Euler', value: 'E460' },
+                { name: 'Gauss', value: 'G200' },
+                { name: 'Hilbert', value: 'H416' },
+                { name: 'Knuth', value: 'K530' },
+                { name: 'Lloyd', value: 'L300' },
+                { name: 'Lukasiewicz', value: 'L222' },
+                { name: 'Ellery', value: 'E460' },
+                { name: 'Ghosh', value: 'G200' },
+                { name: 'Heilbronn', value: 'H416' },
+                { name: 'Kant', value: 'K530' },
+                { name: 'Ladd', value: 'L300' },
+                { name: 'Lissajous', value: 'L222' },
+                { name: 'Wheaton', value: 'W350' },
+                { name: 'Ashcraft', value: 'A226' },
+                { name: 'Burroughs', value: 'B622' },
+                { name: 'Burrows', value: 'B620' },
+                { name: 'O\'Hara', value: 'O600' }
+            ];
+            for (var i = 0, length = SOUNDEX.length; i < length; i++) {
+                expect(soundex(SOUNDEX[i].name)).to.equal(SOUNDEX[i].value);
+            }
+        });
+
+        it('Metaphone', function () {
+            var METAPHONE = [
+                { name: 'Gnu', value: 'N' },
+                { name: 'bigger', value: 'BKR' },
+                { name: 'accuracy', value: 'AKKRS' },
+                { name: 'batch batcher', value: 'BXBXR' }
+                // TODO we need more...
+            ];
+            for (var i = 0, length = METAPHONE.length; i < length; i++) {
+                expect(metaphone(METAPHONE[i].name)).to.equal(METAPHONE[i].value);
+            }
+        });
+
+        it('removeDiacritics', function () {
+            var DIACRITICS = [
+                { name: 'La leçon est terminée', value: 'La lecon est terminee' },
+                { name: 'Cómo está usted', value: 'Como esta usted' },
+                { name: 'można zapoznać się', value: 'mozna zapoznac sie' },
+                { name: 'Z przyjemnością prezentuje Państwu', value: 'Z przyjemnoscia prezentuje Panstwu' }
+                // TODO we need more...
+            ];
+            for (var i = 0, length = DIACRITICS.length; i < length; i++) {
+                expect(removeDiacritics(DIACRITICS[i].name)).to.equal(DIACRITICS[i].value);
+            }
+        });
+
+        it('Array.equals', function () {
+            var ARRAYS = [
+                { a: [1, 2, 3], b: [1, 2, 3] },
+                { a: ['a', 'b', 'c'], b: ['a', 'b', 'c'] }
+            ];
+            for (var i = 0, length = ARRAYS.length; i < length; i++) {
+                expect(ARRAYS[i].a.equals(ARRAYS[i].b)).to.be.true;
+            }
+        });
 
     });
 
-}(this, jQuery));
+}(this));

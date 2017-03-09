37cf71b92ca9cf761473f4a002230e6a9fb68e29
jlchereau <jlchereau@msn.com>
2017-03-09 08:52:26 +0000

    Added skeleton for chargrid https://github.com/kidoju/Kidoju-Widgets/issues/86

--- test/browsers/kidoju.widgets.chargrid.test.js
+++ test/browsers/kidoju.widgets.chargrid.test.js
@@ -15,13 +15,164 @@
     var ui = kendo.ui;
     var CharGrid = ui.CharGrid;
     var FIXTURES = '#fixtures';
-    var DROPZONE1 = '<div id="chargrid1"></div>';
-    var DROPZONE2 = '<div id="chargrid2" data-role="chargrid"></div>';
+    var CHARGRID1 = '<div id="chargrid1"></div>';
+    var CHARGRID2 = '<div id="chargrid2" data-role="chargrid"></div>';
 
     describe('kidoju.widgets.chargrid', function () {
 
-        // TODO
+        before(function () {
+            if (window.__karma__ && $(FIXTURES).length === 0) {
+                $('body').append('<div id="fixtures"></div>');
+            }
+        });
 
+        describe('Availability', function () {
+
+            it('requirements', function () {
+                expect($).not.to.be.undefined;
+                expect(kendo).not.to.be.undefined;
+                expect(kendo.version).to.be.a('string');
+                expect($.fn.kendoCharGrid).to.be.an.instanceof(Function);
+            });
+
+        });
+
+        describe('Initialization', function () {
+
+            it('from code', function () {
+                var element = $(CHARGRID1).appendTo(FIXTURES);
+                var charGrid = element.kendoCharGrid().data('kendoCharGrid');
+                expect(charGrid).to.be.an.instanceof(CharGrid);
+                // expect(element).to.have.class('k-widget');
+                expect(element).to.have.class('kj-chargrid');
+                // TODO expect(charGrid).to.have.property('dataSource').that.is.an.instanceof(kendo.data.DataSource);
+            });
+
+            it('from code with options', function () {
+                var element = $(CHARGRID1).appendTo(FIXTURES);
+                var options = {
+
+                };
+                var charGrid = element.kendoCharGrid().data('kendoCharGrid');
+                expect(charGrid).to.be.an.instanceof(CharGrid);
+                // expect(element).to.have.class('k-widget');
+                expect(element).to.have.class('kj-chargrid');
+                // TODO expect(assetManager).to.have.property('dataSource').that.is.an.instanceof(kendo.data.DataSource);
+            });
+
+            it('from markup', function () {
+                var element = $(CHARGRID2).appendTo(FIXTURES);
+                kendo.init(FIXTURES);
+                var charGrid = element.data('kendoCharGrid');
+                expect(charGrid).to.be.an.instanceof(CharGrid);
+                // expect(element).to.have.class('k-widget');
+                expect(element).to.have.class('kj-chargrid');
+            });
+
+            xit('from markup with attributes', function () {
+                // TODO: AssetManager might be a bit complex to initialize with attributes...
+            });
+
+            afterEach(function () {
+                var fixtures = $(FIXTURES);
+                kendo.destroy(fixtures);
+                fixtures.find('*').off();
+                fixtures.empty();
+            });
+
+        });
+
+        describe('Methods', function () {
+
+            var element;
+            var charGrid;
+            var options = {};
+
+            beforeEach(function () {
+                element = $(CHARGRID1).appendTo(FIXTURES);
+                charGrid = element.kendoCharGrid(options).data('kendoCharGrid');
+            });
+
+            xit('value', function (done) {
+                expect(charGrid).to.be.an.instanceof(CharGrid);
+            });
+
+            xit('setOptions', function () {
+                // TODO especially regarding filters (to be enforced)
+            });
+
+            xit('destroy', function () {
+                expect(charGrid).to.be.an.instanceof(CharGrid);
+                charGrid.destroy();
+                expect(charGrid.element).to.be.empty;
+            });
+
+            afterEach(function () {
+                var fixtures = $(FIXTURES);
+                kendo.destroy(fixtures);
+                fixtures.find('*').off();
+                fixtures.empty();
+            });
+
+        });
+
+        describe('MVVM (and UI interactions)', function () {
+
+            var element;
+            var charGrid;
+            var options = {};
+            var viewModel;
+            var change;
+            var destroy;
+
+            beforeEach(function () {
+                element = $(CHARGRID1).appendTo(FIXTURES);
+                charGrid = element.kendoCharGrid(options).data('kendoCharGrid');
+                viewModel = kendo.observable({
+                    // TODO
+                });
+                change = sinon.spy();
+                destroy = sinon.spy();
+            });
+
+            xit('TODO', function () {
+
+            });
+
+            afterEach(function () {
+                var fixtures = $(FIXTURES);
+                kendo.destroy(fixtures);
+                fixtures.find('*').off();
+                fixtures.empty();
+            });
+
+        });
+
+        describe('Events', function () {
+
+            var element;
+            var charGrid;
+            var options = {};
+            var event;
+
+            beforeEach(function () {
+                element = $(CHARGRID1).appendTo(FIXTURES);
+                charGrid = element.kendoCharGrid(options).data('kendoCharGrid');
+                event = sinon.spy();
+            });
+
+            xit('TODO', function () {
+
+            });
+
+            afterEach(function () {
+                var fixtures = $(FIXTURES);
+                kendo.destroy(fixtures);
+                fixtures.find('*').off();
+                fixtures.empty();
+            });
+
+        });
     });
 
 }(this, jQuery));

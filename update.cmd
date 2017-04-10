7ec3652478e3af1ea1502d9b44e0af8d0ea2f371
jlchereau <jlchereau@msn.com>
2017-04-10 17:50:31 +0100

    Merged update scripts

--- update.cmd
+++ update.cmd
@@ -1,3 +1,15 @@
 REM Copy Kidoju webfonts
 XCOPY ..\Kidoju.WebFonts\dist\fonts\*.* .\src\styles\fonts\ /C /E /I /R /Y
 ATTRIB +R .\src\styles\fonts\*
+
+REM update Mathquill
+ATTRIB -R .\src\js\vendor\mathquill\mathquill.js
+COPY ..\..\Explorations\mathquill\build\mathquill.js .\src\js\vendor\mathquill\mathquill.js /Y
+ATTRIB +R .\src\js\vendor\mathquill\mathquill.js
+
+ATTRIB -R .\src\styles\vendor\mathquill\mathquill.css
+COPY ..\..\Explorations\mathquill\build\mathquill.css .\src\styles\vendor\mathquill\mathquill.css /Y
+ATTRIB +R .\src\styles\vendor\mathquill\mathquill.css
+
+XCOPY ..\..\Explorations\mathquill\build\fonts .\src\styles\vendor\mathquill\fonts /C /E /I /R /Y
+ATTRIB +R .\src\styles\vendor\mathquill\fonts\*.* /S

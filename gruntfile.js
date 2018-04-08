7f2eadf738b5df298cd02072813d434faaf14eab
jlchereau <jlchereau@msn.com>
2018-04-08 14:34:07 +0100

    Add stylelint configuration

--- gruntfile.js
+++ gruntfile.js
@@ -3,36 +3,40 @@
  * Sources at https://github.com/Memba/Kidoju-Platform
  */
 
-/* jslint node: true */
-/* jshint node: true */
-
-/* And because of kendo_lint */
-/* jscs:disable requireCamelCaseOrUpperCaseIdentifiers */
-
-'use strict';
-
-module.exports = function (grunt) {
-
+module.exports = grunt => {
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
-        watch: {
-            files: ['<%= jshint.files %>'],
-            tasks: ['lint', 'test']
-        },
         eslint: {
-            files: ['templates/*.es6', 'src/js/**/*.es6', 'test/browser/**/*.es6'],
+            files: [
+                'gruntfile.js',
+                'templates/*.es6',
+                'src/js/**/*.es6',
+                'test/**/*.es6'
+            ],
             options: {
                 config: '.eslintrc'
             }
         },
         jscs: {
-            files: ['gruntfile.js', 'src/js/kidoju.*.js', 'src/js/window.*.js', 'src/js/messages/kidoju.*.js', 'test/browser/*.js', 'test/zombie/*.js'],
+            files: ['src/js/**/*.js', 'test/**/*.js'],
             options: {
                 config: '.jscsrc'
             }
         },
+        jsdoc: {
+            dist: {
+                src: ['src/**/*.es6', 'src/**/*.js', 'README.md'],
+                options: {
+                    destination: 'docs',
+                    template:
+                        'node_modules/grunt-jsdoc/node_modules/ink-docstrap/template',
+                    configure:
+                        'node_modules/grunt-jsdoc/node_modules/ink-docstrap/template/jsdoc.conf.json'
+                }
+            }
+        },
         jshint: {
-            files: ['gruntfile.js', 'src/js/kidoju.*.js', 'src/js/window.*.js', 'src/js/messages/kidoju.*.js', 'test/browser/*.js', 'test/zombie/*.js'],
+            files: ['src/js/**/*.js', 'test/**/*.js'],
             options: {
                 // options here to override JSHint defaults
                 jshintrc: '.jshintrc'
@@ -51,16 +55,8 @@ module.exports = function (grunt) {
             files: ['src/js/kidoju.*.js']
         },
         */
-        csslint: {
-            strict: {
-                options: {
-                    import: 2
-                },
-                src: ['src/styles/kidoju.*.css']
-            }
-        },
-        // TODO: Consider linting html too
-        mocha: { // Tests running in a browser (phantomJS)
+        mocha: {
+            // Tests running in a browser (phantomJS)
             browser: {
                 options: {
                     // debug: true,
@@ -73,7 +69,8 @@ module.exports = function (grunt) {
                 src: ['test/browser/*.test.html']
             }
         },
-        mochaTest: { // Test running in nodeJS (supertest, zombie, ...)
+        mochaTest: {
+            // Test running in nodeJS (supertest, zombie, ...)
             zombie: {
                 options: {
                     debug: true,
@@ -82,98 +79,14 @@ module.exports = function (grunt) {
                     timeout: 10000,
                     ui: 'bdd'
                 },
-                src: ['test/node/zombie/kidoju.*.test.js']
+                src: ['test/node/zombie/*.test.js']
             }
         },
-        clean: ['dist/', 'docs/'],
-        copy: {
-            /*
-            dist: {
-                options: {
-                    process: function(src, filepath) {
-                        // Replace with min versions
-                        var ret;
-                        if (filepath === 'src/js/init.js'){
-                            ret = src
-                                .replace(/DEBUG[\s]*=[\s]*true/gm, 'DEBUG = false')
-                                .replace(/init.js/gm, 'init.min.js');
-                        }
-                        return ret || src;
-                    },*/
-            // noProcess: ['**/*.{png,gif,jpg,ico,psd}'] // otherwise images are corrupted
-            /*},
-                files: [
-            // { cwd: 'src/styles', src: ['fonts/**'], dest: 'dist/styles', expand: true },
-                    { cwd: 'src/styles', src: ['images/**'], dest: 'dist/styles', expand: true }
-                ]
-            },*/
-            vendor: {
-                files: [{
-                    expand: true,
-                    cwd: 'src/js/vendor/',
-                    src: '**',
-                    dest: 'dist/js/vendor/'
-                }]
-            }
-        },
-        concat: {
-            css: {
-                src: ['src/styles/kidoju*.css'],
-                dest: 'dist/styles/<%= pkg.filename %>.css'
-            },
-            js: {
-                options: {
-                    separator: '\n;\n',
-                    process: function (src /*, filepath*/) {
-                        // Replace DEBUG = true with DEBUG = false
-                        return src.replace(/DEBUG[\s]*=[\s]*true/gm, 'DEBUG = false');
-                    }
-                },
-                src: [/*'src/js/vendor/*.js',*/ 'src/js/kidoju*.js'],
-                dest: 'dist/js/<%= pkg.filename %>.js'
-            }
-        },
-        uglify: {
+        stylelint: {
             options: {
-                banner: '// <%= pkg.name %> <%= pkg.version %> built on <%= grunt.template.today("dd-mm-yyyy") %> - <%= pkg.copyright %>\n',
-                sourceMap: true,
-                sourceMapName: 'dist/js/<%= pkg.filename %>.min.map'
+                configFile: '.stylelintrc'
             },
-            dist: {
-                files: {
-                    'dist/js/<%= pkg.filename %>.min.js': ['<%= concat.js.dest %>']
-                }
-            }
-        },
-        cssmin: {
-            widgets: {
-                options: {
-                    banner: '/* <%= pkg.name %> <%= pkg.version %> built on <%= grunt.template.today("dd-mm-yyyy") %> - <%= pkg.copyright %> */\n'
-                },
-                files: {
-                    'dist/styles/<%= pkg.filename %>.min.css': ['<%= concat.css.dest %>']
-                }
-            }
-        },
-        imagemin: {
-            widgets: {
-                files: [{
-                    expand: true,
-                    cwd: 'src/styles/images/',
-                    src: ['**/*.{png,jpg,gif}'],
-                    dest: 'dist/styles/images/'
-                }]
-            }
-        },
-        jsdoc : {
-            dist : {
-                src: ['src/**/*.js', 'README.md'],
-                options: {
-                    destination: 'docs',
-                    template : 'node_modules/grunt-jsdoc/node_modules/ink-docstrap/template',
-                    configure : 'node_modules/grunt-jsdoc/node_modules/ink-docstrap/template/jsdoc.conf.json'
-                }
-            }
+            src: ['src/styles/**/*.{css,less,scss}']
         }
         /*
         yuidoc: {
@@ -191,33 +104,17 @@ module.exports = function (grunt) {
         */
     });
 
-    // Watching
-    grunt.loadNpmTasks('grunt-contrib-watch');
-
-    // Linting
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    // grunt.loadNpmTasks('grunt-contrib-yuidoc');
     grunt.loadNpmTasks('grunt-eslint');
     grunt.loadNpmTasks('grunt-jscs');
-    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-jsdoc');
     // grunt.loadNpmTasks('grunt-kendo-lint');
-    grunt.loadNpmTasks('grunt-contrib-csslint');
-
-    // Testing
     grunt.loadNpmTasks('grunt-mocha');
     grunt.loadNpmTasks('grunt-mocha-test');
+    grunt.loadNpmTasks('grunt-stylelint');
 
-    // Building
-    grunt.loadNpmTasks('grunt-contrib-clean');
-    grunt.loadNpmTasks('grunt-contrib-copy');
-    grunt.loadNpmTasks('grunt-contrib-concat');
-    grunt.loadNpmTasks('grunt-contrib-uglify');
-    grunt.loadNpmTasks('grunt-contrib-cssmin');
-    grunt.loadNpmTasks('grunt-contrib-imagemin');
-    grunt.loadNpmTasks('grunt-jsdoc');
-    // grunt.loadNpmTasks('grunt-contrib-yuidoc');
-
-    grunt.registerTask('lint', ['jscs', 'jshint', 'csslint']);
-    grunt.registerTask('lint2', ['eslint', 'csslint']);
+    grunt.registerTask('lint', ['eslint', 'jscs', 'jshint', 'stylelint']);
     grunt.registerTask('test', ['mocha']); // , 'mochaTest']);
-    grunt.registerTask('build', ['clean', 'copy', 'concat', 'uglify', 'cssmin', 'imagemin', 'jsdoc']);
-    grunt.registerTask('default', ['lint', 'test', 'build']);
+    grunt.registerTask('default', ['lint', 'test']);
 };

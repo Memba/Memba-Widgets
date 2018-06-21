e72e21c6feb0a6c4ac6c4f8d6a5b818d944058be
jlchereau <jlchereau@msn.com>
2018-06-21 10:42:40 +0100

    Upgraded CodeMirror from v5.38.0 to v5.39.0

--- src/js/vendor/codemirror/update.cmd
+++ src/js/vendor/codemirror/update.cmd
@@ -2,7 +2,7 @@ REM goto current directory
 cd /d %~dp0
 
 REM set directory where CodeMirror is built from sources
-set DIR=C:\Users\jlche\Desktop\CodeMirror-5.38.0
+set DIR="%USERPROFILE%\Desktop\CodeMirror-5.39.0"
 
 REM \addon\lint\
 COPY "%DIR%\addon\lint\javascript-lint.js" ".\addon\lint\javascript-lint.js"

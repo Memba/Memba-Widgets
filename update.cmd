7252a1905cb9f846f4c90a7bf73b6c3a6f08c2c9
jlchereau <jlchereau@msn.com>
2018-09-11 21:02:19 +0100

    Moved (and update) original src/js/data files to Kidoju-Server

--- update.cmd
+++ update.cmd
@@ -2,15 +2,37 @@ REM goto current directory
 cd /d %~dp0
 
 REM Copy babel-polyfill
-XCOPY .\node_modules\babel-polyfill\dist .\test\vendor /C /E /I /R /Y
+XCOPY .\node_modules\@babel\polyfill\dist .\test\vendor /C /E /I /R /Y
 
-REM Copy from Kidoju-Server
+REM Copy common files from Kidoju-Server
 XCOPY ..\Kidoju.Server\src\js\common .\src\js\common /C /E /I /R /Y
 ATTRIB +R .\src\js\common\pongodb.*.es6
 ATTRIB +R .\src\js\common\window.assert.es6
 ATTRIB +R .\src\js\common\window.constants.es6
 ATTRIB +R .\src\js\common\window.logger.es6
 ATTRIB +R .\src\js\common\window.util.es6
+
+REM Copy data files from Kidoju-Server
+ATTRIB -R .\src\js\data\*.*
+COPY ..\Kidoju.Server\src\js\data\datasources.base.es6 .\src\js\data /Y
+COPY ..\Kidoju.Server\src\js\data\datasources.page.es6 .\src\js\data /Y
+COPY ..\Kidoju.Server\src\js\data\datasources.pagecomponent.es6 .\src\js\data /Y
+COPY ..\Kidoju.Server\src\js\data\models.base.es6 .\src\js\data /Y
+COPY ..\Kidoju.Server\src\js\data\models.page.es6 .\src\js\data /Y
+COPY ..\Kidoju.Server\src\js\data\models.pagecomponent.es6 .\src\js\data /Y
+COPY ..\Kidoju.Server\src\js\data\models.stream.es6 .\src\js\data /Y
+ATTRIB +R .\src\js\data\*.*
+
+REM Copy test files from Kidoju-Server
+ATTRIB -R .\test\browser\data\*.*
+REM COPY ..\Kidoju.Server\test\browser\data\datasources.base.test.* .\test\browser\data /Y
+REM COPY ..\Kidoju.Server\test\browser\data\datasources.page.test.* .\test\browser\data /Y
+REM COPY ..\Kidoju.Server\test\browser\data\datasources.pagecomponent.test.* .\test\browser\data /Y
+COPY ..\Kidoju.Server\test\browser\data\models.base.test.* .\test\browser\data /Y
+COPY ..\Kidoju.Server\test\browser\data\models.page.test.* .\test\browser\data /Y
+COPY ..\Kidoju.Server\test\browser\data\models.pagecomponent.test.* .\test\browser\data /Y
+COPY ..\Kidoju.Server\test\browser\data\models.stream.test.* .\test\browser\data /Y
+ATTRIB +R .\test\browser\data\*.*
 
 REM Copy Kidoju-WebFonts
 XCOPY ..\Kidoju.WebFonts\dist\fonts\*.* .\src\styles\fonts\ /C /E /I /R /Y

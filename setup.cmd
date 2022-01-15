8bd311bc4461bf447515350f7653a7a5d67d3774
jlchereau <jlchereau@msn.com>
2022-01-15 13:07:49 +0000

    Updated setup

--- setup.cmd
+++ setup.cmd
@@ -1,19 +1,50 @@
 cd /d %~dp0
 call "%ProgramFiles%\nodejs\nodevars.bat"
 REM update npm, which is a requirement for some modules
-npm install -g npm
-REM grunt command in terminal mode
-npm install -g grunt-cli
-REM code coverage with istanbul
-npm install -g istanbul
-REM karma command in terminal mode
-npm install -g karma-cli
-REM mocha command in terminal mode
-REM npm install -g mocha
-REM nodemon to run webapp while debugging browser code
-npm install -g nodemon
-REM webpack for builds
-npm install -g webpack
-npm install -g webpack-dev-server
-REM install dependencies listed in package.json
-npm install
+call npm install -g npm
+
+REM appium
+call npm install -g appium-doctor
+call npm install -g appium
+
+REM aws
+call npm install -g aws-cdk
+
+REM cordova
+call npm install -g cordova-simulate
+call npm install -g cordova
+
+REM expo cli
+REM call npm install -g expo-cli
+
+REM forever -> pm2
+call npm install -g forever
+
+REM grunt cli
+call npm install -g grunt-cli
+
+REM karma cli
+call npm install -g karma-cli
+
+REM ncu
+call npm install -g npm-check-updates
+
+REM sass
+call npm install -g sass
+
+REM selenium
+call npm install -g selenium-standalone
+call selenium-standalone install
+
+REM sloc
+call npm install -g sloc
+
+REM Typescript
+call npm install -g typescript
+
+REM weinre
+REM call npm install -g weinre
+
+REM install all local modules in package.json
+set NODE_ENV=development
+call npm install
--- setup.sh
+++ setup.sh
@@ -2,21 +2,53 @@
 
 # change directory to script location
 cd "$(dirname "$0")"
+# call "%ProgramFiles%\nodejs\nodevars.bat"
 
 # update npm, which is a requirement for some modules
 npm install -g npm
-# grunt command in terminal mode
+
+# appium
+npm install -g appium-doctor
+npm install -g appium
+
+# aws
+npm install -g aws-cdk
+
+# cordova
+npm install -g cordova-simulate
+npm install -g cordova
+
+# expo cli
+# npm install -g expo-cli
+
+# forever -> pm2
+npm install -g forever
+
+# grunt cli
 npm install -g grunt-cli
-# code coverage with istanbul
-npm install -g istanbul
-# karma command in terminal mode
+
+# karma cli
 npm install -g karma-cli
-# mocha command in terminal mode
-# npm install -g mocha
-# nodemon to run webapp while debugging browser code
-npm install -g nodemon
-# webpack for builds
-npm install -g webpack
-npm install -g webpack-dev-server
-# install dependencies listed in package.json
+
+# ncu
+npm install -g npm-check-updates
+
+# sass
+npm install -g sass
+
+# selenium
+npm install -g selenium-standalone
+selenium-standalone install
+
+# sloc
+npm install -g sloc
+
+# Typescript
+npm install -g typescript
+
+# weinre
+# npm install -g weinre
+
+# install all local modules in package.json
+NODE_ENV=development
 npm install
